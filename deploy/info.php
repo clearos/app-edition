cee12bcd4a82667c24a1732b04e58d9ac6e13885
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-06-22 15:39:20 +0000

    - Prep for 6.3.0 Beta 1

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'edition';
-$app['version'] = '1.1.6';
+$app['version'] = '1.2.2';
 $app['release'] = '1';
 $app['vendor'] = 'ClearCenter';
 $app['packager'] = 'ClearCenter';
--- packaging/app-edition.spec
+++ packaging/app-edition.spec
@@ -1,7 +1,7 @@
 
 Name: app-edition
 Epoch: 1
-Version: 1.1.6
+Version: 1.2.2
 Release: 1%{dist}
 Summary: Edition Manager
 License: Proprietary
