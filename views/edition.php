02c4e0c3800c5859b28a2c9d112f56637888e02e
Benjamin Chambers <bchambers@clearfoundation.com>
2014-11-27 15:46:32 -0600

    Updated selector page...wip

--- views/edition.php
+++ views/edition.php
@@ -36,10 +36,35 @@ if ($professional_already_installed) {
 // TODO: integrate radio set into theme
 // TODO: integrate text size and other hard-coded elements in theme_field_radio_set_item
 
-$banner = "<h2 style='font-size: 1.8em; color: #909090; width: 687px;'>Thank You for Choosing ClearOS</h2>";
-$banner .= "<img style='float: right;' src='" . clearos_app_htdocs('edition') . "/thanks.png' alt=''>";
-$banner .= "<p style='font-size: 1.2em; line-height: 20px;'>ClearOS Community Edition is supported by a community of volunteers donating their time and knowledge via the ClearFoundation.  This support model may not be the best solution for your business or organisation.  Please consider all factors (technical skill, number of users, data sensitivity, resources etc.) when considering using ClearOS Community Edition in a commercial, professional, educational or large-scale deployment.";
-$banner .= "<p style='font-size: 1.2em; line-height: 20px;'>ClearCenter offers a commercially supported version of ClearOS called ClearOS Professional Edition.  If you would like to upgrade to a 30-day evaluation of ClearOS Professional Edition, please select this option.</p>";
+echo "<h2 style='font-size: 1.8em; color: #909090; width: 687px;'>Thank You for Choosing ClearOS</h2>";
+echo row_open();
+echo column_open(6);
+echo "<p style='font-size: 1.2em; line-height: 20px;'>ClearOS Community Edition is supported by a community of volunteers donating their time and knowledge via the ClearFoundation.  This support model may not be the best solution for your business or organisation.  Please consider all factors (technical skill, number of users, data sensitivity, resources etc.) when considering using ClearOS Community Edition in a commercial, professional, educational or large-scale deployment.";
+echo "<p style='font-size: 1.2em; line-height: 20px;'>ClearCenter offers a commercially supported version of ClearOS called ClearOS Professional Edition.  If you would like to upgrade to a 30-day evaluation of ClearOS Professional Edition, please select this option.</p>";
+echo column_close();
+echo column_open(6);
+
+echo row_open();
+
+echo column_open(6);
+echo box_open('Community Edition');
+echo box_content(image('network.svg', array('size' => "150x150", 'class' => 'theme-center-text')));
+echo box_footer('footer-community', "Install Community", array('class' => 'edition-option-footer'));
+echo box_close();
+echo column_close();
+
+echo column_open(6);
+echo box_open('Professional Edition');
+echo box_content(image('server.svg', array('size' => "150x150", 'class' => 'theme-center-text')));
+echo box_footer('footer-pro', anchor_custom('#', 'Select Upgrade', 'high', null, array('id' => 'select-professional')), array('class' => 'edition-option-footer'));
+echo box_close();
+echo column_close();
+
+echo row_close();
+
+echo column_close();
+echo row_close();
+
 
 $community_label = "<span style='font-size: 13px;'>Install ClearOS Community</span>";
 $community_options['image'] = clearos_app_htdocs('edition') . '/community_logo.png';
@@ -50,25 +75,3 @@ $professional_options['image'] = clearos_app_htdocs('edition') . '/professional_
 $professional_options['orientation'] = 'horizontal';
 
 $options['orientation'] = 'horizontal';
-
-$read_only = FALSE;
-
-///////////////////////////////////////////////////////////////////////////////
-// Form
-///////////////////////////////////////////////////////////////////////////////
-
-echo form_open('edition', array('id' => 'edition_form'));
-echo form_header(lang('edition_select_edition'));
-
-echo form_banner($banner);
-echo field_radio_set(
-    '',
-    array(
-        field_radio_set_item('community', 'edition', $community_label, TRUE, $read_only, $community_options),
-        field_radio_set_item('professional', 'edition', $professional_label, FALSE, $read_only, $professional_options)
-    ),
-    array('orientation' => 'horizontal')
-);
-
-echo form_footer();
-echo form_close();
