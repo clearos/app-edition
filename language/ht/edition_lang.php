e9ab0285a36d3f1a9a9242d0f6dfb0a5df63d061
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:17:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'avote Mizajou';
+$lang['edition_app_description'] = 'Manadjè a edisyon bay yon pwosesis seleksyon yo chwazi vèsyon an bon pou anviwònman ou ak bidjè.';
+$lang['edition_app_name'] = 'edisyon Manadjè';
+$lang['edition_confirm_edition_change'] = 'Èske ou sèten ou vle chwazi yon diferan ClearOS edisyon? Tanpri asire w ke ou gen yon abònman valab disponib nan re-enskri sistèm nan. Si ou se fin, tanpri kontakte sipò teknik.';
+$lang['edition_currently_running'] = 'vèsyon lojisyèl Kouran';
+$lang['edition_downgrade'] = 'pant';
+$lang['edition_edition'] = 'edisyon';
+$lang['edition_exec_summary_business'] = 'Pwofesyonèl sipòte IT platfòm pou biznis ki vle pou misyon pou minimize risk pou yo D &#39;.';
+$lang['edition_exec_summary_community'] = 'Gratis kounye a, free pou tout tan. Gen entansyon pou ki gen eksperyans du Linux oswa moun ki renmen "Tinker".';
+$lang['edition_exec_summary_home'] = 'Ede gaye pawòl la ak pataje ClearOS Kay edisyon ak zanmi w ak fanmi.';
+$lang['edition_no_edition_selected'] = 'Tanpri chwazi ki edisyon ou ta renmen yo enstale.';
+$lang['edition_no_reset'] = 'Sa a edisyon pa kapab Reyajiste, tanpri kontakte ekip la sipò pou asistans.';
+$lang['edition_not_available_during_testing'] = 'Pa disponib pandan alfa / beta.';
+$lang['edition_not_available'] = 'Pa disponib.';
+$lang['edition_proceed_to_registration'] = 'Ou pral kounye a dwe reyorante resous nan enskripsyon sistèm re-dispozisyon sistèm ou an.';
+$lang['edition_select_edition'] = 'chwazi edisyon';
+$lang['edition_summary_business'] = 'ClearOS biznis edisyon se devlope ak mentni pa ClearCenter Inc. Si ou ap chèche pou yon sekirite, ki estab ak sipòte platfòm IT, edisyon an Biznis gen aksè a apps san konte biznis-espesifik epi li sipòte pa yon gwoup de ki gen eksperyans Linux admin, devlopè ak espesyalis deplwaman.';
+$lang['edition_summary_community'] = 'ClearOS Kominote edisyon se sipòte pa yon kominote nan volontè bay tan yo ak konesans atravè ClearFoundation la. modèl sipò Sa a pa kapab solisyon a pi bon pou biznis ou oswa òganizasyon. Tanpri konsidere tout faktè (konpetans teknik, kantite itilizatè, done sansiblite, resous elatriye) lè w ap konsidere lè l sèvi avèk ClearOS Kominote edisyon nan yon komèsyal yo, pwofesyonèl, edikasyon oswa gwo-echèl deplwaman.';
+$lang['edition_summary_home'] = 'ClearOS Kay edisyon se ideyal pou anviwònman kay kote moun vle plis karakteristik ak entegre sèvis nwaj pase sa yo ki yo, se tipikman ki ofri pa passerelles ISP-bay oswa lakay-vize aparèy rezo-a.';
+$lang['edition_system_registration'] = 'Sistèm Enskripsyon';
+$lang['edition_thank_you'] = 'Mèsi pou Chwazi ClearOS';
+$lang['edition_updating'] = 'Software Mizajou nan Pwogrè';
+$lang['edition_upgrade_edition'] = 'Upgrade edisyon';
+$lang['edition_upgrade'] = 'Upgrade';
