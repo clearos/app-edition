b64ffce68ed8329af64490119af02c62d2bd3c2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:19:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'გამოცემა მენეჯერი უზრუნველყოფს შერჩევის პროცესში არჩევანის უფლება ვერსია თქვენი გარემო და ბიუჯეტი.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = ' გთხოვთ დარწმუნდეთ, რომ თქვენ გაქვთ სწორი გამოწერა ხელმისაწვდომია გადააფორმოს სისტემა. ';
+$lang['edition_currently_running'] = 'მიმდინარე პროგრამული უზრუნველყოფის ვერსიის';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'პროფესიონალურ დონეზე მხარი დაუჭირეს პლატფორმა ბიზნესის, რომელთაც სურთ, რათა შეამციროს რისკი downtime.';
+$lang['edition_exec_summary_community'] = ' განკუთვნილი გამოცდილი Linux admins და მათთვის, ვისაც სურს "tinker".';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'ეს გამოცემა არ შეიძლება გადატვირთვა, გთხოვთ, დაგვიკავშირდეთ მხარდაჭერა გუნდი დახმარებას.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition არის შემუშავებული და შენარჩუნებული ClearCenter Inc. ';
+$lang['edition_summary_community'] = 'ClearOS Community Edition მხარს უჭერს თანამეგობრობის მოხალისეები აბარებენ მათი დრო და ცოდნა მეშვეობით ClearFoundation.  ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition იდეალურია სახლის გარემოში, სადაც პირებს სურთ მეტი ფუნქციები და ინტეგრირებული Cloud მომსახურება ვიდრე ის, რომ, როგორც წესი, გათვალისწინებული ISP გაცემული გეითვეი ან სახლში მიზნობრივი ქსელური მოწყობილობების.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'გმადლობთ ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'განახლება';
