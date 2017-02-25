df66cddca969afaa749ccd27e411ee53a9399604
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:16:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Édition gestionnaire';
+$lang['edition_confirm_edition_change'] = '  ';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Édition';
+$lang['edition_exec_summary_business'] = 'Professionnellement pris en charge la plate-forme informatique pour les entreprises qui veulent réduire le risque de temps d&#39;arrêt.';
+$lang['edition_exec_summary_community'] = ' ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'Cette édition ne peut pas être remise à zéro, s&#39;il vous plaît contacter l&#39;équipe de soutien à l&#39;assistance.';
+$lang['edition_not_available_during_testing'] = 'Non disponible pendant alpha / bêta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = ' Si vous êtes à la recherche d&#39;une plate-forme informatique sécurisée, stable et soutenu, la Business Edition a accès à des applications spécifiques à l&#39;activité exclusive et est soutenu par un groupe d&#39;administration de Linux, les développeurs et les spécialistes de déploiement.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition est soutenu par une communauté de bénévoles donnant de leur temps et de connaissances via le ClearFoundation.  S&#39;il vous plaît prendre en considération tous les facteurs (compétences techniques, le nombre d&#39;utilisateurs, la sensibilité des données, ressources, etc.) lors de l&#39;examen en utilisant ClearOS Community Edition dans un déploiement commercial, professionnel, éducatif ou à grande échelle.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Merci d&#39;avoir choisi ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Surclassement';
