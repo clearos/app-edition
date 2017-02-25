94b990bf057588aff6e4b6b162b3783ac6d85291
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:23:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'afbreken bijwerken';
+$lang['edition_app_description'] = 'De Edition Manager biedt een selectieproces om de juiste versie voor uw omgeving en budget te kiezen.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = 'Weet je zeker dat je een ander ClearOS editie selecteren? Zorg ervoor dat u een geldig abonnement beschikbaar voor opnieuw te registreren het systeem. Als u twijfelt, neem dan contact op met de technische ondersteuning.';
+$lang['edition_currently_running'] = 'Huidige softwareversie';
+$lang['edition_downgrade'] = 'downgrade';
+$lang['edition_edition'] = 'Editie';
+$lang['edition_exec_summary_business'] = 'Professioneel ondersteund IT-platform voor bedrijven die willen het risico op downtime te minimaliseren.';
+$lang['edition_exec_summary_community'] = 'nu vrij, gratis voor altijd. Bestemd voor ervaren Linux admins of degenen die graag "tinker".';
+$lang['edition_exec_summary_home'] = 'De hulp spreidde het woord en deel ClearOS Home Edition met je vrienden en familie.';
+$lang['edition_no_edition_selected'] = 'Selecteer welke editie u wilt installeren.';
+$lang['edition_no_reset'] = 'Deze editie kan niet worden gereset, neem dan contact op met het ondersteuningsteam voor hulp.';
+$lang['edition_not_available_during_testing'] = 'Niet beschikbaar tijdens alpha / beta.';
+$lang['edition_not_available'] = 'Niet beschikbaar.';
+$lang['edition_proceed_to_registration'] = 'Je wordt nu doorgestuurd naar registratiesysteem opnieuw bepaling van uw systeem.';
+$lang['edition_select_edition'] = 'Selecteer Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition is ontwikkeld en wordt onderhouden door ClearCenter Inc. Als u op zoek bent naar een veilig, stabiel en ondersteunde IT-platform, de Business Edition heeft toegang tot exclusieve zaken-specifieke apps en wordt ondersteund door een groep van ervaren Linux admin, ontwikkelaars en implementatie-specialisten.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition wordt ondersteund door een community van vrijwilligers doneren van hun tijd en kennis via de ClearFoundation. Deze ondersteuning model misschien niet de beste oplossing voor uw bedrijf of organisatie. Gelieve rekening houden met alle factoren (technische vaardigheid, het aantal gebruikers, data gevoeligheid, resources etc.) bij het overwegen van het gebruik van ClearOS Community Edition in een commercieel, professioneel, educatief of grootschalige toepassing.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideaal voor thuis omgevingen waar mensen willen meer functies en geïntegreerde cloud-diensten dan die doorgaans worden geleverd door-ISP uitgegeven gateways of-home gerichte netwerkapparaten.';
+$lang['edition_system_registration'] = 'Systeemregistratie';
+$lang['edition_thank_you'] = 'Dank u voor het kiezen van ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
