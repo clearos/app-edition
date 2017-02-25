c5cf995d1c5732d44de6b70af59f283259aace28
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:22:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = ' Jekk jogħġbok kun ċert li għandek abbonament valida disponibbli li jerġgħu jirreġistraw is-sistema. ';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professjonalment appoġġati pjattaforma tal-IT għan-negozji li jixtiequ li jimminimizzaw ir-riskju ta &#39;perijodi ta&#39; waqfien.';
+$lang['edition_exec_summary_community'] = ' Maħsuba għall admins Linux esperjenza jew dawk li jħobbu "Tinker".';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'Mhux disponibbli matul alfa / beta.';
+$lang['edition_not_available'] = 'Mhux disponibbli.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = ' ';
+$lang['edition_summary_community'] = 'ClearOS Edizzjoni Komunitarja hija appoġġata minn komunità ta &#39;voluntiera donazzjoni ħin u l-għarfien tagħhom permezz tal-ClearFoundation.  ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Grazzi għall-għażla ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Taġġorna';
