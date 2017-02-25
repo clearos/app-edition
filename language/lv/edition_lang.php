c910e9c5be2e3f06d206970b5046587faf1a4187
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:20:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'pārtraukt Update';
+$lang['edition_app_description'] = 'Izdevums pārvaldnieks nodrošina atlases procesu, lai izvēlēties pareizo versiju savu vidi un budžetu.';
+$lang['edition_app_name'] = 'izdevums vadītājs';
+$lang['edition_confirm_edition_change'] = 'Vai jūs tiešām vēlaties, lai izvēlētos citu ClearOS izdevums? Lūdzu, pārliecinieties, vai jums ir derīga abonements pieejams pārreģistrēt sistēmu. Ja neesat pārliecināts, lūdzu, sazinieties ar tehnisko atbalstu.';
+$lang['edition_currently_running'] = 'Pašreizējā programmatūras versija';
+$lang['edition_downgrade'] = 'Atkāpšanās';
+$lang['edition_edition'] = 'izdevums';
+$lang['edition_exec_summary_business'] = 'Profesionāli atbalsta IT platformu uzņēmumiem, kuri vēlas, lai mazinātu risku, ka dīkstāves.';
+$lang['edition_exec_summary_community'] = 'Bezmaksas tagad, bezmaksas uz visiem laikiem. Paredzēts pieredzējušiem Linux admins vai tiem, kam patīk "lāpīt".';
+$lang['edition_exec_summary_home'] = 'Palīdzētu izplatīt vārdu un dalīties ClearOS Home Edition ar draugiem un ģimeni.';
+$lang['edition_no_edition_selected'] = 'Lūdzu izvēlēties, kuras izdevums vēlaties instalēt.';
+$lang['edition_no_reset'] = 'Šis izdevums nevar atiestatīt, lūdzu, sazinieties ar atbalsta dienestu.';
+$lang['edition_not_available_during_testing'] = 'Nav pieejams alfa / beta laikā.';
+$lang['edition_not_available'] = 'Nav pieejams.';
+$lang['edition_proceed_to_registration'] = 'Jūs tagad tiks novirzīts uz sistēmas reģistrācijas atkārtoti noteikums jūsu sistēmā.';
+$lang['edition_select_edition'] = 'Izvēlieties Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition ir izstrādājusi un uztur ClearCenter Inc. Ja jūs meklējat drošu, stabilu un atbalstīja to platformas, Business Edition ir pieejama ekskluzīvi biznesa specifisku lietotņu un to atbalsta grupa pieredzējušu Linux admin, attīstītājiem un izvēršanas speciālisti.';
+$lang['edition_summary_community'] = 'ClearOS Kopienas Edition atbalsta kopienas brīvprātīgo ziedot savu laiku un zināšanas, izmantojot ClearFoundation. Šis atbalsts modelis var nebūt labākais risinājums jūsu uzņēmumam vai organizācijai. Lūdzu, apsveriet visus faktorus (tehniskās iemaņas, lietotāju skaitu, datu jutīguma, resursi uc) apsverot izmantojot ClearOS Kopienas Edition komerciālā, profesionālā, izglītības vai liela mēroga izmantojumu.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'sistēma Reģistrācijas';
+$lang['edition_thank_you'] = 'Paldies, ka izvēlējāties ClearOS';
+$lang['edition_updating'] = 'Software Update Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
