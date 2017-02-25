39301dd23e08ce9b899e9e3804c93fbea594ca0d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:28:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Güncelleştirme abort';
+$lang['edition_app_description'] = 'Baskı Yöneticisi çevre ve bütçeniz için doğru sürümünü seçmek için bir seçim süreci sağlar.';
+$lang['edition_app_name'] = 'Baskı Yöneticisi';
+$lang['edition_confirm_edition_change'] = 'Eğer farklı bir ClearOS sürümü seçmek istediğinize emin misiniz? sistemi yeniden kaydettirmek için kullanılabilir, geçerli bir abonelik olduğundan emin olun. Eğer emin değilseniz, teknik desteğe başvurun.';
+$lang['edition_currently_running'] = 'Geçerli yazılım sürümü';
+$lang['edition_downgrade'] = 'bozmak';
+$lang['edition_edition'] = 'Baskı';
+$lang['edition_exec_summary_business'] = 'Profesyonelce kesinti riskini en aza indirmek isteyen işletmeler için bilişim platformu desteklenmektedir.';
+$lang['edition_exec_summary_community'] = 'Şimdi özgür, sonsuza kadar özgür. deneyimli Linux yöneticileri ya da "tamirci" isteyenler için tasarlanmıştır.';
+$lang['edition_exec_summary_home'] = 'kelime yaymak yardımcı olacak ve arkadaşlarınızla ve ailenizle ClearOS Home Edition paylaşın.';
+$lang['edition_no_edition_selected'] = 'Kurmak istediğiniz sürümü seçin.';
+$lang['edition_no_reset'] = 'Bu baskı sıfırlama olamaz, yardım için destek ekibine başvurun.';
+$lang['edition_not_available_during_testing'] = 'alfa / beta sırasında geçerli değildir.';
+$lang['edition_not_available'] = 'Müsait değil.';
+$lang['edition_proceed_to_registration'] = 'Eğer sisteminizi şimdi yeniden ön hazırlığını sistem kayıt yönlendirileceksiniz.';
+$lang['edition_select_edition'] = 'Sürüm seçin';
+$lang['edition_summary_business'] = 'ClearOS Business Edition geliştirilen ve ClearCenter A.Ş. tarafından yapılmaktadır Eğer güvenli, istikrarlı ve desteklenen BT platformu arıyorsanız, Business Edition özel bir iş-özel uygulamalara erişimi vardır ve deneyimli Linux Admin, geliştiriciler ve dağıtım uzman bir grup tarafından desteklenmektedir.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition ClearFoundation aracılığıyla zaman ve bilgi bağış gönüllü bir topluluk tarafından desteklenmektedir. Bu destek modeli işiniz veya kuruluşunuz için en iyi çözüm olmayabilir. Bir ticari, profesyonel, eğitim ya da büyük ölçekli dağıtım ClearOS Community Edition kullanarak göz önüne alındığında tüm faktörler (teknik beceri, kullanıcı sayısı, veri duyarlılığı, kaynaklar vs.) dikkate alınız.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition bireyler daha fazla özellik ve tipik ISS tarafından verilen ağ geçitleri veya ev hedefli ağ cihazları tarafından sağlanan olanlar daha entegre bulut hizmetleri isteyen ev ortamları için idealdir.';
+$lang['edition_system_registration'] = 'sistem Kayıt';
+$lang['edition_thank_you'] = 'ClearOS seçimi için teşekkür';
+$lang['edition_updating'] = 'Devam Yazılım Güncellemesi';
+$lang['edition_upgrade_edition'] = 'Edition&#39;ı yükseltme';
+$lang['edition_upgrade'] = 'yükseltmek';
