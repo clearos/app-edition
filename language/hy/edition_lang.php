e739e64b06d0463615b65d952439c8e2885ce00f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:13:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Վիժել Update';
+$lang['edition_app_description'] = 'The Edition տնօրեն տրամադրում է ընտրության գործընթացը ընտրելու ճիշտ տարբերակը Ձեր միջավայրում եւ բյուջե.';
+$lang['edition_app_name'] = 'Edition մենեջեր';
+$lang['edition_confirm_edition_change'] = 'Համոզված եք, որ ցանկանում եք ընտրել այլ ClearOS խմբագրությունը: Խնդրում ենք համոզվեք, որ դուք ունեք մի վավեր բաժանորդագրություն մատչելի վերագրանցել համակարգը: Եթե ​​դուք վստահ չեք, խնդրում ենք դիմել տեխնիկական աջակցություն:';
+$lang['edition_currently_running'] = 'Ընթացիկ ծրագրային տարբերակը';
+$lang['edition_downgrade'] = 'նվազեցնում';
+$lang['edition_edition'] = 'հրատարակություն';
+$lang['edition_exec_summary_business'] = 'Պրոֆեսիոնալ աջակցում էր հարթակ է բիզնեսի համար, ովքեր ցանկանում են նվազագույնի հասցնել ռիսկը downtime.';
+$lang['edition_exec_summary_community'] = 'Ազատ հիմա անվճար հավերժ. Նախատեսված է փորձառու Linux Admins կամ նրանց, ովքեր ցանկանում են «Tinker».';
+$lang['edition_exec_summary_home'] = 'Օգնություն տարածել բառը եւ կիսվել ClearOS Գլխավոր թողարկում ձեր ընկերների եւ ընտանիքի հետ:';
+$lang['edition_no_edition_selected'] = 'Խնդրում ենք ընտրել, որոնք Edition ցանկանում եք տեղադրել.';
+$lang['edition_no_reset'] = 'Այս հրատարակությունը չի կարող լինել reset, խնդրում ենք դիմել աջակցության թիմի աջակցության համար:';
+$lang['edition_not_available_during_testing'] = 'Հասանելի չէ ժամանակ ալֆա / բետա.';
+$lang['edition_not_available'] = 'Անհասանելի.';
+$lang['edition_proceed_to_registration'] = 'Դուք այժմ Վերահղված է համակարգի գրանցման կրկին չտրամադրման ձեր համակարգը.';
+$lang['edition_select_edition'] = 'Ընտրել Edition';
+$lang['edition_summary_business'] = 'ClearOS Բիզնես Edition, որը մշակվել է եւ պահպանվում է ClearCenter Inc. Եթե ​​դուք փնտրում են անվտանգ, կայուն եւ աջակցում ՏՏ պլատֆորմի վրա, Business Edition ունի մուտք դեպի բացառիկ բիզնես կոնկրետ apps եւ աջակցում է մի խումբ փորձառու Linux admin, ծրագրավորողներ եւ տեղակայման մասնագետների:';
+$lang['edition_summary_community'] = 'ClearOS Համայնքի Edition աջակցում է համայնքի կամավորների նվիրաբերեց իրենց ժամանակը եւ գիտելիքները միջոցով ClearFoundation. Այս աջակցությունը մոդելը չի ​​կարող լինել լավագույն լուծումը ձեր բիզնեսի կամ կազմակերպության: Խնդրում ենք հաշվի առնել բոլոր գործոնները (տեխնիկական հմտություն, օգտատերերի թիվն, տվյալների զգայունության, ռեսուրսներ եւ այլն), երբ, հաշվի առնելով, օգտագործելով ClearOS Համայնքի Edition է առեւտրային, մասնագիտական, կրթական, կամ լայնածավալ տեղակայման:';
+$lang['edition_summary_home'] = 'ClearOS Home Edition իդեալական է տնային միջավայրում, որտեղ մարդիկ ուզում են ավելի շատ հատկանիշներ եւ ինտեգրված ամպային ծառայությունները, քան նրանք, որոնք, որպես կանոն, տրամադրվում է պրովայդերների կողմից տրված gateways կամ տնային թիրախավորված ցանցային սարքերի:';
+$lang['edition_system_registration'] = 'System Գրանցման';
+$lang['edition_thank_you'] = 'Շնորհակալություն ընտրելու ClearOS';
+$lang['edition_updating'] = 'Software Update ընթացքում է';
+$lang['edition_upgrade_edition'] = 'Թարմացնելու Edition';
+$lang['edition_upgrade'] = 'բարելավում';
