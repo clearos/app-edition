2b62c553e2deec62efba1f3bac6d1cf3ff4bb6da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:20:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'nutraukti Update';
+$lang['edition_app_description'] = 'Leidinys direktorius teikia atrankos procesą pasirinkti tinkamą versiją savo aplinką ir biudžetą.';
+$lang['edition_app_name'] = 'leidimas direktorius';
+$lang['edition_confirm_edition_change'] = 'Ar tikrai norite pasirinkti kitą ClearOS leidimas? Prašome įsitikinkite, kad turite galiojantį prenumerata prieinama perregistruoti sistemą. Jei nesate tikri, susisiekite su techninės pagalbos.';
+$lang['edition_currently_running'] = 'Dabartinis programinės įrangos versija';
+$lang['edition_downgrade'] = 'downgrade';
+$lang['edition_edition'] = 'leidimas';
+$lang['edition_exec_summary_business'] = 'Profesionaliai palaiko platformą įmonėms, kurie nori sumažinti prastovų riziką.';
+$lang['edition_exec_summary_community'] = 'Nemokamai dabar nemokamai amžinai. Skirtas patyrusių Linux administratoriai arba tiems, kurie mėgsta "Tinker".';
+$lang['edition_exec_summary_home'] = 'Pagalba skleisti žodį ir dalintis ClearOS Home Edition su savo draugais ir šeima.';
+$lang['edition_no_edition_selected'] = 'Prašome pasirinkti kuri versija norėtumėte įdiegti.';
+$lang['edition_no_reset'] = 'Šis leidimas negali būti iš naujo, kreipkitės į palaikymo komandą pagalbos.';
+$lang['edition_not_available_during_testing'] = 'Nėra alfa / beta metu.';
+$lang['edition_not_available'] = 'Nepasiekiamas.';
+$lang['edition_proceed_to_registration'] = 'Jūs dabar bus nukreiptas į sistemos registracijos naujo teikimo sistemą.';
+$lang['edition_select_edition'] = 'Pasirinkite Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition "yra sukurta ir palaikoma ClearCenter Inc. Jei jūs ieškote saugaus, stabilaus ir ją rėmė platforma, Business Edition "turi prieigą prie išskirtinių verslo konkrečių apps ir yra palaikoma patyrusių Linux admin, kūrėjams ir diegimo specialistų grupė.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition yra paremta savanorių aukojant savo laiką ir žinias per ClearFoundation bendruomenei. Ši parama modelis gali būti geriausias sprendimas jūsų verslui ar organizacijai. svarstant naudojant ClearOS Bendrijos Edition komercinę, profesinę, švietimo ar didelio masto diegimo Prašome apsvarstyti visus veiksnius (techninių įgūdžių, vartotojų skaičius, duomenų jautrumą, išteklių ir kt.)';
+$lang['edition_summary_home'] = 'ClearOS Home Edition yra idealiai tinka namų aplinkoje, kur žmonės nori daugiau funkcijų ir integruotą debesys paslaugų, nei tie, kurie paprastai teikia IPT išduotas vartai ar namų orientuojasi tinklo įrenginių.';
+$lang['edition_system_registration'] = 'sistemos Registracija';
+$lang['edition_thank_you'] = 'Dėkojame, kad pasirinkote ClearOS';
+$lang['edition_updating'] = 'Programinės įrangos naujinimas iš "Progress"';
+$lang['edition_upgrade_edition'] = 'Atnaujinkite Edition';
+$lang['edition_upgrade'] = 'Patobulinti';
