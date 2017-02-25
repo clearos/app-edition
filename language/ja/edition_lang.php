c2820c0289afc849949511e13059a6c1236169ea
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:19:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = ' あなたは、システムを再登録するために利用可能な有効なサブスクリプションを持っていることを確認してください。 あなたがわからない場合は、テクニカルサポートに連絡してください。';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'プロダウンタイムのリスクを最小限にしたい企業のためのITプラットフォームをサポートしていました。';
+$lang['edition_exec_summary_community'] = ' ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'このエディションはリセットできません、支援のためのサポートチームにお問い合わせください。';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'エディションを選択';
+$lang['edition_summary_business'] = 'ClearOSビジネス版がClearCenter社によって開発され、維持されています ';
+$lang['edition_summary_community'] = '  、商用プロ、教育や大規模展開でClearOSコミュニティ版を使用して考慮した場合、すべての要素（技術的なスキル、ユーザー数、データの機密性、リソース等）をご検討ください。';
+$lang['edition_summary_home'] = 'ClearOS Home Editionには、個人は、通常、ISP-発行ゲートウェイまたは家庭対象のネットワークデバイスから提供されているものよりも優れた機能と統合されたクラウドサービスをしたい家庭環境に最適です。';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'アップグレード';
