ffffacebb2901d062fd67403c241842f7b8adf97
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:15:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort uuendus';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = '  Kui sa ei ole kindel, võtke ühendust tehnilise toega.';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professionaalselt toetatud IT platvorm ettevõtetele, kes soovivad vähendada ohtu seisuaega.';
+$lang['edition_exec_summary_community'] = ' ';
+$lang['edition_exec_summary_home'] = 'Aidata levitada ja jagada ClearOS Home Edition oma sõprade ja perega.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition on välja töötatud ja haldab ClearCenter Inc. Kui otsite turvaline, stabiilne ja toetatud IT platvormi Business Edition on juurdepääs eksklusiivne äri-spetsiifilisi rakendusi ja toetab rühm kogenud Linux admin, arendajad ja kasutuselevõtu spetsialistid.';
+$lang['edition_summary_community'] = '  ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Täname ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'versiooniuuendusväljaande';
+$lang['edition_upgrade'] = 'Upgrade';
