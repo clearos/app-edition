ee3d61465a590e07ba3fcea7b99ac6f5bb0e1c74
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:21:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = '  Raha toa ianao ka antoka, dia mifandraisa fanohanana ara-teknika.';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'manambany';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professionally supported IT platform for businesses who want to minimize the risk of downtime.';
+$lang['edition_exec_summary_community'] = 'Free ankehitriny, free mandrakizay. ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'Zavatra fanontana tsy afaka ny ho Reset, dia mifandraisa ny fanohanana ekipa ho fanampiana.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = ' ';
+$lang['edition_summary_community'] = ' Modely io fanohanana Mety tsy ho ny vahaolana tsara indrindra noho ny raharaham-barotra na ny fikambanana. ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Misaotra Anao noho ny mifidy ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
