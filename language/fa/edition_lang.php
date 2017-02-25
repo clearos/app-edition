a34231a9536431762ace5651b3903ed405397de3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:23:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = ' لطفا مطمئن شوید که شما یک اشتراک معتبر در دسترس دوباره ثبت نام سیستم. اگر شما مطمئن نیستید، لطفا با پشتیبانی فنی.';
+$lang['edition_currently_running'] = 'نسخه فعلی نرم افزار';
+$lang['edition_downgrade'] = 'جمع و جور کردن';
+$lang['edition_edition'] = 'چاپ';
+$lang['edition_exec_summary_business'] = 'حرفه ای پلت فرم آن را برای کسب و کار که می خواهند برای به حداقل رساندن خطر ابتلا به از کار افتادگی پشتیبانی می شود.';
+$lang['edition_exec_summary_community'] = 'رایگان در حال حاضر، رایگان برای همیشه. نظر گرفته شده برای مدیران لینوکس را تجربه و یا کسانی که دوست دارند به "سرهم بندی کردن".';
+$lang['edition_exec_summary_home'] = 'کمک به گسترش این کلمه و به اشتراک گذاری ClearOS نسخه خانگی با دوستان و خانواده خود را.';
+$lang['edition_no_edition_selected'] = 'لطفا انتخاب کنید که نسخه شما می خواهم به نصب کنید.';
+$lang['edition_no_reset'] = 'این نسخه نمی توان تنظیم مجدد، لطفا برای کمک با تیم پشتیبانی.';
+$lang['edition_not_available_during_testing'] = 'در طول آلفا / بتا در دسترس نیست.';
+$lang['edition_not_available'] = 'در دسترس نیست.';
+$lang['edition_proceed_to_registration'] = 'شما در حال حاضر به ثبت نام سیستم هدایت می شوید تا دوباره ارائه سیستم شما.';
+$lang['edition_select_edition'] = 'انتخاب کنید نسخه';
+$lang['edition_summary_business'] = 'ClearOS کسب و کار نسخه است توسعه و نگهداری توسط شرکت ClearCenter اگر شما به دنبال برای یک پلت فرم امن، باثبات و پشتیبانی فناوری اطلاعات، نسخه کسب و کار دسترسی به برنامه های کسب و کار خاص منحصر به فرد است و توسط یک گروه از تجربه محیط مدیریت لینوکس، توسعه دهندگان و متخصصان استقرار پشتیبانی می شود.';
+$lang['edition_summary_community'] = 'ClearOS انجمن نسخه است که توسط یک جامعه از داوطلبان اهدای زمان و دانش خود را از طریق ClearFoundation پشتیبانی می شود. این مدل پشتیبانی ممکن است بهترین راه حل برای کسب و کار شما است. لطفا همه عوامل (مهارت های فنی، تعداد کاربران، حساسیت داده ها، منابع و غیره) که با توجه به استفاده از ClearOS انجمن نسخه در یک استقرار تجاری، حرفه ای، آموزشی و یا در مقیاس بزرگ در نظر بگیرند.';
+$lang['edition_summary_home'] = 'ClearOS نسخه خانگی ایده آل برای محیط خانه که در آن افراد می خواهند از ویژگی های بیشتر و خدمات ابر یکپارچه از کسانی که به طور معمول توسط دروازه صادر ISP و یا دستگاه های شبکه خانگی را هدف قرار ارائه شده است.';
+$lang['edition_system_registration'] = 'ثبت نام سیستم';
+$lang['edition_thank_you'] = 'با تشکر از شما برای انتخاب ClearOS';
+$lang['edition_updating'] = 'به روز رسانی نرم افزار در حال';
+$lang['edition_upgrade_edition'] = 'ارتقاء نسخه';
+$lang['edition_upgrade'] = 'ارتقا';
