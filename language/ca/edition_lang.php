832e3bafeedc0f7d2dac4bf7e2c2a2b99c2368fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:14:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'actualització d&#39;avortar';
+$lang['edition_app_description'] = 'L&#39;Administrador Edition proporciona un procés de selecció per a triar la versió correcta per al seu entorn i pressupost.';
+$lang['edition_app_name'] = 'Manager edition';
+$lang['edition_confirm_edition_change'] = 'Esteu segur que voleu seleccionar una edició diferent ClearOS? Si us plau assegureu-vos que té una subscripció vàlida disponible per tornar a registrar el sistema. Si no sabeu, si us plau, poseu-vos en contacte amb el suport tècnic.';
+$lang['edition_currently_running'] = 'versió actual del programari';
+$lang['edition_downgrade'] = 'degradar';
+$lang['edition_edition'] = 'edició';
+$lang['edition_exec_summary_business'] = 'recolzat professionalment plataforma informàtica per a les empreses que volen minimitzar el risc de temps d&#39;inactivitat.';
+$lang['edition_exec_summary_community'] = 'Lliure ara, lliure per sempre. Destinat als administradors experimentats de Linux o els que els agrada "Tinker".';
+$lang['edition_exec_summary_home'] = 'Ajuda a difondre i compartir ClearOS Home Edition amb els seus amics i familiars.';
+$lang['edition_no_edition_selected'] = 'Si us plau seleccioni l&#39;edició que voleu instal·lar.';
+$lang['edition_no_reset'] = 'Aquesta edició no es pot restablir, si us plau poseu-vos en contacte amb l&#39;equip de suport a l&#39;assistència.';
+$lang['edition_not_available_during_testing'] = 'No està disponible durant alfa / beta.';
+$lang['edition_not_available'] = 'No disponible.';
+$lang['edition_proceed_to_registration'] = 'Ara serà redirigit a un registre del sistema per tornar al seu sistema de provisió.';
+$lang['edition_select_edition'] = 'Seleccioneu Edició';
+$lang['edition_summary_business'] = 'ClearOS Business Edition és desenvolupat i mantingut per ClearCenter Inc. Si vostè està buscant una plataforma segura, estable i amb el suport, l&#39;edició del negoci té accés a aplicacions exclusives i específiques del negoci i és recolzat per un grup d&#39;administrador experimentat de Linux, els desenvolupadors i especialistes en implementació.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition està suportat per una comunitat de voluntaris que donen el seu temps i coneixements a través de la ClearFoundation. Aquest model de suport pot no ser la millor solució per a la seva empresa o organització. Favor de considerar tots els factors (habilitat tècnica, nombre d&#39;usuaris, la sensibilitat de dades, recursos, etc.) en considerar l&#39;ús de ClearOS Community Edition en un desplegament comercial, professional, educatiu o de gran escala.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition és ideal per a entorns domèstics on les persones volen més funcions i serveis en el núvol integrats que aquells que són típicament proveïts per passarel·les ISP-emesa o dispositius de xarxa a casa dirigida.';
+$lang['edition_system_registration'] = 'sistema de registre';
+$lang['edition_thank_you'] = 'Gràcies per triar ClearOS';
+$lang['edition_updating'] = 'Actualització de programari en curs';
+$lang['edition_upgrade_edition'] = 'edició de la millora';
+$lang['edition_upgrade'] = 'modernització';
