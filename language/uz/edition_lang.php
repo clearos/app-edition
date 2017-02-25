09dfbfbbc5c08b500b232d235c086c4563ff3907
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:28:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'yangilash Abort';
+$lang['edition_app_description'] = 'Edition Manager atrof-muhit va byudjet uchun to&#39;g&#39;ri versiyasini tanlash uchun tanlash jarayonini ta&#39;minlaydi.';
+$lang['edition_app_name'] = 'Edition menejeri';
+$lang['edition_confirm_edition_change'] = 'Agar bir xil ClearOS nashri tanlash tashlamoqchimisiz? Agar tizimini qayta ro&#39;yxatdan mumkin bo&#39;lgan amal obuna bor ishonch hosil qiling. Ishonch hosil bo&#39;lmasa, texnik qo&#39;llab-quvvatlash murojaat qiling.';
+$lang['edition_currently_running'] = 'Amaldagi dasturiy ta&#39;minot versiyasi';
+$lang['edition_downgrade'] = 'pasaytirish';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professional uzilishlar xavfini kamaytirish uchun kerakli korxonalar uchun IT platforma qo&#39;llab-quvvatladi.';
+$lang['edition_exec_summary_community'] = 'endi bepul, abadiy bepul. tajribali Linux rahbarlari yoki "tinker" kabi kishilar uchun mo&#39;ljallangan.';
+$lang['edition_exec_summary_home'] = 'so&#39;z tarqaldi yordam va do&#39;stlaringiz va oilangiz bilan ClearOS Home edition baham.';
+$lang['edition_no_edition_selected'] = 'o&#39;rnatish uchun qaysi nashri tanlang.';
+$lang['edition_no_reset'] = 'Bu nashr reset bo&#39;lishi mumkin emas, yordam uchun qo&#39;llab-quvvatlash jamoasi murojaat qiling.';
+$lang['edition_not_available_during_testing'] = 'alfa / beta davomida mavjud emas.';
+$lang['edition_not_available'] = 'Mavjud emas.';
+$lang['edition_proceed_to_registration'] = 'Siz endi tizimi-rizq qayta tizimi ro&#39;yxatdan kirasiz.';
+$lang['edition_select_edition'] = 'edition tanlang';
+$lang['edition_summary_business'] = 'ClearOS Biznes Edition ishlab chiqilgan va ClearCenter Inc. tomonidan muhofaza qilinadi ';
+$lang['edition_summary_community'] = 'ClearOS Community Edition ClearFoundation orqali ularning vaqt va bilim ehson ko&#39;ngillilar hamjamiyat tomonidan qo&#39;llab-quvvatlanadi. Ushbu qo&#39;llab-quvvatlash model ishingiz yoki tashkilot uchun eng yaxshi yechim bo&#39;lishi mumkin emas. a, tijorat professional, ta&#39;lim yoki keng ko&#39;lamli tarqatish ClearOS Community Edition yordamida hisobga olib, barcha omillarni (texnik mahorat, foydalanuvchilar soni, ma&#39;lumotlar o&#39;ta sezgir, resurslar va boshqalar) ko&#39;rib qiling.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Ro&#39;yxatdan';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
