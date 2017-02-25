52dd751fad805f9b99bc31544dfde6190e43ae85
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:25:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'prerušiť aktualizácie';
+$lang['edition_app_description'] = 'Edícia Manager poskytuje výberové konanie zvoliť správnu verziu pre váš životné prostredie a rozpočet.';
+$lang['edition_app_name'] = 'vydanie Manažér';
+$lang['edition_confirm_edition_change'] = 'Ste si istí, že chcete vybrať iný ClearOS vydanie? Prosím, skontrolujte, či máte platný predplatné k dispozícii znovu zaregistrovať systém. Ak si nie ste istí, obráťte sa na technickú podporu.';
+$lang['edition_currently_running'] = 'Aktuálna verzia softvéru';
+$lang['edition_downgrade'] = 'degradovať';
+$lang['edition_edition'] = 'Vydanie';
+$lang['edition_exec_summary_business'] = 'Profesionálne podporované IT platformu pre podnikateľov, ktorí chcú, aby sa minimalizovalo riziko poruchy.';
+$lang['edition_exec_summary_community'] = 'teraz zadarmo, zadarmo navždy. Určený pre skúsených administrátorov Linux alebo tých, ktorí radi "Drotár".';
+$lang['edition_exec_summary_home'] = 'Pomôcť šíriť slovo a zdieľať ClearOS Home Edition so svojimi priateľmi a rodinou.';
+$lang['edition_no_edition_selected'] = 'Prosím, vyberte, ktoré vydanie chcete nainštalovať.';
+$lang['edition_no_reset'] = 'Táto edícia nemožno vynulovať, obráťte sa na tím podpory pre pomoc.';
+$lang['edition_not_available_during_testing'] = 'Nie je k dispozícii v priebehu alfa / beta.';
+$lang['edition_not_available'] = 'Nie sú k dispozícii.';
+$lang['edition_proceed_to_registration'] = 'Teraz budete presmerovaný na registračný systém re-provision počítač.';
+$lang['edition_select_edition'] = 'vyberte Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition je vyvíjaný a udržiavaný ClearCenter Inc. Ak hľadáte pre bezpečný, stabilný a podporované IT platformu Business Edition má prístup k exkluzívnym aplikáciám obchodne špecifické a je podporovaný skupinou, ktorý má skúsenosti Linux admin, vývojári a nasadenie špecialistov.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition je podporovaný komunitou dobrovoľníkov venujú svoj čas a znalosti prostredníctvom ClearFoundation. Tento model pomoc nemusí byť najlepším riešením pre vašu firmu či organizáciu. Prosím, vziať do úvahy všetky faktory (technické zručnosti, počet užívateľov, citlivosť dát, zdroje atď.), Keď uvažuje o nasadení ClearOS Community Edition v obchodnej, odborné, vzdelávacie alebo vo veľkom meradle nasadenie.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition je ideálny pre domáce prostredie, kde jednotlivci chcú viac funkcií a integrované cloudové služby ako tie, ktoré sú zvyčajne poskytované brány ISP vydaný alebo domáce cielené sieťovými zariadeniami.';
+$lang['edition_system_registration'] = 'systém registrácie';
+$lang['edition_thank_you'] = 'Ďakujeme, že ste si vybrali ClearOS';
+$lang['edition_updating'] = 'Aktualizácia softvéru in Progress';
+$lang['edition_upgrade_edition'] = 'upgrade Edition';
+$lang['edition_upgrade'] = 'upgrade';
