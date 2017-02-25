707d7f0ec3e8a74042b7652255f7e1f86b465c62
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:22:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Update зогсоох';
+$lang['edition_app_description'] = 'Edition менежер өөрийн орчны, төсвийн зөв хувилбарыг сонгох нь сонгон шалгаруулах үйл ажиллагаа болно.';
+$lang['edition_app_name'] = 'Edition менежер';
+$lang['edition_confirm_edition_change'] = 'Хэрэв та өөр ClearOS хэвлэлийг сонгох нь итгэлтэй байна уу? Хэрэв та зөв захиалах боломжтой системийг дахин бүртгэх эсэхээ шалгаарай уу. Хэрэв та мэдэхгүй байгаа бол, техникийн дэмжлэг холбоо барина уу.';
+$lang['edition_currently_running'] = 'Одоогийн програм хангамжийн хувилбар';
+$lang['edition_downgrade'] = 'буурахыг';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Мэргэжлийн унасан байх эрсдэлийг багасгахын тулд хүсэж байгаа бизнес эрхлэгчдэд IT платформ дэмжсэн.';
+$lang['edition_exec_summary_community'] = 'одоо Чөлөөт үүрд үнэгүй. туршлагатай линукс Админ буюу "Tinker" дуртай хүмүүст зориулагдсан.';
+$lang['edition_exec_summary_home'] = 'үгийг тархсан нь тусал болон таны найз нөхөд, гэр бүлийнхэнтэйгээ хамт ClearOS Нүүр хуудас Edition хуваалцаж байна.';
+$lang['edition_no_edition_selected'] = 'Хэрэв та аль хэвлэл суулгахыг хүсэж байгаа сонгоно уу.';
+$lang['edition_no_reset'] = 'Энэ нь хэвлэл нь дахин эхлүүлэх байж болохгүй, туслалцаа дэмжлэг үзүүлэх багтай холбоо барина уу.';
+$lang['edition_not_available_during_testing'] = 'альфа / бета үед боломжгүй.';
+$lang['edition_not_available'] = 'боломжтой байх.';
+$lang['edition_proceed_to_registration'] = 'Та одоо заалтыг дахин таны системийг систем бүртгэлд уруу дахин чиглүүлэгдэх болно.';
+$lang['edition_select_edition'] = 'Edition сонгоно уу';
+$lang['edition_summary_business'] = 'ClearOS Бизнесийн Edition боловсруулж, ClearCenter Inc. хангагдаж байна Хэрэв та, найдвартай, тогтвортой, дэмжиж IT платформ хайж байгаа бол, Бизнесийн Edition онцгой бизнесийн онцлог програм хандах боломжтой байдаг бөгөөд туршлагатай линукс админы, хөгжүүлэгч, байрлуулалт мэргэжилтнүүдийн бүлэг дэмжиж байна.';
+$lang['edition_summary_community'] = 'ClearOS Олон нийтийн хэвлэл ClearFoundation дамжуулан өөрсдийн цаг зав, мэдлэг өргөл сайн дурын олон нийтийн дэмжигдсэн байдаг. Энэ нь дэмжлэг загвар нь таны бизнест, байгууллагын хувьд хамгийн сайн шийдэл байж болох юм. нь, арилжааны, мэргэжил, боловсролын болон их хэмжээний журамд орох ClearOS олон нийтийн хэвлэл ашиглан авч үзэхдээ бүх хүчин зүйлийг (техникийн ур чадвар, хэрэглэгчдийн тоо, мэдээлэл мэдрэмжийн, нөөц гэх мэт) авч уу.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'Системийн бүртгэл';
+$lang['edition_thank_you'] = 'ClearOS сонгох тань баярлалаа';
+$lang['edition_updating'] = 'Хөгжил дэвшил Software Update';
+$lang['edition_upgrade_edition'] = 'Edition шинэчлэх';
+$lang['edition_upgrade'] = 'Upgrade';
