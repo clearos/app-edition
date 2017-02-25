dc5c5b933373af3fc93f04cb761026e1a6f02021
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:25:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = 'Ma hubtaa in aad rabto in aad ka dooro a edition kala duwan ClearOS? Fadlan hubi in aad leedahay Times oo sax ah la heli karo si dib-u-diiwaan nidaamka sameeyaan. ';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'hoos';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professionally supported IT platform for businesses who want to minimize the risk of downtime.';
+$lang['edition_exec_summary_community'] = 'hadda Free, free weligiis. ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'Waxaad hadda la wareejiyey doonaan in ay diiwaan nidaamka dib-u-bixinta habka.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = ' Haddii aad raadinayso ammaan ah, oo deggan iyo taageeray madal IT ah, Edition Business uu leeyahay helitaanka apps ganacsi-gaar gaar ah iyo waxa taageeray koox ka mid ah soo maray admin Linux, horumarinta iyo takhasuska ciidamada.';
+$lang['edition_summary_community'] = ' model taageero Tani waxaa laga yaabaa in aanay noqon xalka ugu fiican ganacsigaaga ama urur. ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
