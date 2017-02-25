b8d9ee810e3045b62ab20de6addd1a2457b7f2ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:18:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager na-enye a nhọrọ usoro na-ahọrọ nri version gị gburugburu ebe obibi na mmefu ego.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = 'Ì ji n&#39;aka na ị chọrọ iji họrọ a dị iche iche ClearOS mbipụta? Biko jide n&#39;aka na ị nwere nti ndenye aha dị re-aha na usoro. Ọ bụrụ na ị na-amaghị, biko kpọtụrụ nkwado ndị teknuzu.';
+$lang['edition_currently_running'] = 'Ugbu software version';
+$lang['edition_downgrade'] = 'downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professionally akwado IT n&#39;elu ikpo okwu maka ulo oru na-achọ na-ebelata ihe ize ndụ nke Downtime.';
+$lang['edition_exec_summary_community'] = 'Free now, free ruo mgbe ebighị ebi. Zubere maka nwere Linux admins ma ọ bụ ndị na-amasị "tinker".';
+$lang['edition_exec_summary_home'] = 'Nyere agbasa okwu ma na-eso ClearOS Home Edition na ndị enyi gị na ndị ezinụlọ.';
+$lang['edition_no_edition_selected'] = 'Biko họrọ nke mbipụta ị ga-achọ ka ịwụnye.';
+$lang['edition_no_reset'] = 'Nke a mbipụta nwere ike ịbụ nrụpụta, biko kpọtụrụ nkwado otu maka enyemaka.';
+$lang['edition_not_available_during_testing'] = 'Adịghị n&#39;oge Alfa / beta.';
+$lang['edition_not_available'] = 'Adịghị.';
+$lang['edition_proceed_to_registration'] = 'Ị ga-now ike ibugharị ka usoro ndebanye re-ndokwa gị usoro.';
+$lang['edition_select_edition'] = 'họrọ Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition na-mepụtara na nọgidere na ClearCenter Inc. Ọ bụrụ na ị na-achọ a ala, anụ ma na-akwado IT n&#39;elu ikpo okwu, na Business Edition nwere ohere nanị azụmahịa-kpọmkwem ngwa ọdịnala na a na-akwado otu ìgwè nke nwere Linux admin, mmepe na nkenye ọnọdụ ọkachamara.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition a na-akwado a obodo ndị ọrụ afọ ofufo inye onyinye oge ha na ihe ọmụma site na ClearFoundation. Nke a support nlereanya nwere ike ịbụ ihe ngwọta kasị mma maka azụmahịa gị ma ọ bụ nzukọ. Biko tụlee ihe niile ihe (ọrụaka nkà, ọnụ ọgụgụ nke ndị ọrụ, data uche, akụ wdg) mgbe na-ewere iji ClearOS Community Edition na a azụmahịa, ọrụ, izi ma ọ bụ nnukwu-ọnụ ọgụgụ nkenye ọnọdụ.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition bụ ezigbo maka n&#39;ụlọ gburugburu ebe ndị mmadụ n&#39;otu n&#39;otu na-achọ ọzọ atụmatụ na integrated ígwé ojii ọrụ karịa ndị nke na-Elezie-nye site ISP-agbala gateways ma ọ bụ n&#39;ụlọ-ezubere iche netwọk ngwaọrụ.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Ekele dịrị Gị maka Ịhọrọ ClearOS';
+$lang['edition_updating'] = 'Software Update na Ọganihu';
+$lang['edition_upgrade_edition'] = 'kwalite Edition';
+$lang['edition_upgrade'] = 'nweta nkwalite';
