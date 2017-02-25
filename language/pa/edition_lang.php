2e6fc4cf9f2147495bc165a7b4ae5dec70f2d213
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:23:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'ਅਧੂਰਾ ਅੱਪਡੇਟ';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = ' ਕਿਰਪਾ ਕਰਕੇ ਤੁਹਾਨੂੰ ਇੱਕ ਜਾਇਜ਼ ਗਾਹਕੀ ਸਿਸਟਮ ਨੂੰ ਮੁੜ-ਰਜਿਸਟਰ ਕਰਨ ਲਈ ਉਪਲੱਬਧ ਹੈ ਇਹ ਯਕੀਨੀ ਬਣਾਉਣ. ';
+$lang['edition_currently_running'] = 'ਮੌਜੂਦਾ ਸਾਫਟਵੇਅਰ ਵਰਜਨ';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'ਐਡੀਸ਼ਨ';
+$lang['edition_exec_summary_business'] = 'ਪੇਸ਼ੇਵਰ ਕਾਰੋਬਾਰ ਜੋ ਖਾਸ ਦੇ ਖਤਰੇ ਨੂੰ ਘੱਟ ਕਰਨ ਲਈ ਚਾਹੁੰਦੇ ਹੋ ਲਈ ਇਸ ਪਲੇਟਫਾਰਮ ਨੂੰ ਸਹਿਯੋਗ.';
+$lang['edition_exec_summary_community'] = ' ';
+$lang['edition_exec_summary_home'] = 'ਸ਼ਬਦ ਨੂੰ ਫੈਲ ਮਦਦ ਕਰਨ ਅਤੇ ਤੁਹਾਡੇ ਦੋਸਤ ਅਤੇ ਪਰਿਵਾਰ ਦੇ ਨਾਲ ClearOS ਮੁੱਖ ਐਡੀਸ਼ਨ ਸ਼ੇਅਰ.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'ਇਹ ਐਡੀਸ਼ਨ ਰੀਸੈਟ ਹੋ ਨਾ ਹੋ ਸਕਦਾ ਹੈ, ਸਹਾਇਤਾ ਦੇ ਲਈ ਸਹਾਇਤਾ ਟੀਮ ਨਾਲ ਸੰਪਰਕ ਕਰੋ ਜੀ.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'ਤੁਹਾਨੂੰ ਹੁਣ-ਪ੍ਰਬੰਧ ਨੂੰ ਮੁੜ ਕਰਨ ਲਈ ਆਪਣੇ ਸਿਸਟਮ ਸਿਸਟਮ ਰਜਿਸਟਰੇਸ਼ਨ ਨੂੰ ਮੁੜ ਨਿਰਦੇਸ਼ਤ ਕੀਤਾ ਜਾਵੇਗਾ.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = 'ClearOS ਵਪਾਰ ਐਡੀਸ਼ਨ ਵਿਕਸਤ ਅਤੇ ClearCenter ਇੰਕ ਦੇ ਕੇ ਰੱਖਿਆ ਜਾ ਰਿਹਾ ਹੈ ';
+$lang['edition_summary_community'] = '  ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'ਤਰੱਕੀ ਵਿੱਚ ਸਾਫਟਵੇਅਰ ਅੱਪਡੇਟ';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
