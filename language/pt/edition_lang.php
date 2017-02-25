aeb1deba595c4b5227abcbdcd0037ded5112d1d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:24:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'abortar Atualização';
+$lang['edition_app_description'] = 'A Edição Manager fornece um processo de seleção para escolher a versão correta para o seu ambiente e orçamento.';
+$lang['edition_app_name'] = 'Manager edition';
+$lang['edition_confirm_edition_change'] = 'Tem certeza de que deseja selecionar uma edição diferente ClearOS? Por favor, verifique se você tem uma assinatura válida disponível para re-registrar o sistema. Se não tiver certeza, entre em contato com o suporte técnico.';
+$lang['edition_currently_running'] = 'versão atual do software';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edição';
+$lang['edition_exec_summary_business'] = 'Profissionalmente apoiada plataforma de TI para as empresas que querem minimizar o risco de tempo de inatividade.';
+$lang['edition_exec_summary_community'] = 'Livre agora, livre para sempre. Destina-se a administradores Linux experientes ou aqueles que gostam de "mexer".';
+$lang['edition_exec_summary_home'] = 'Ajude a espalhar a palavra e compartilhar ClearOS Home Edition com os seus amigos e familiares.';
+$lang['edition_no_edition_selected'] = 'Por favor, selecione qual edição você gostaria de instalar.';
+$lang['edition_no_reset'] = 'Esta edição não pode ser reposto, entre em contato com a equipe de suporte para obter assistência.';
+$lang['edition_not_available_during_testing'] = 'Não disponível durante alpha / beta.';
+$lang['edition_not_available'] = 'Não disponível.';
+$lang['edition_proceed_to_registration'] = 'Agora vai ser redirecionado para registro de sistema de re-disposição seu sistema.';
+$lang['edition_select_edition'] = 'Selecione Edição';
+$lang['edition_summary_business'] = 'ClearOS Business Edition é desenvolvido e mantido pela ClearCenter Inc. Se você estiver procurando por uma plataforma de TI segura, estável e apoiado, o Business Edition tem acesso a aplicações específicas de negócios exclusivos e é apoiado por um grupo de administrador Linux experientes, desenvolvedores e especialistas em implantação.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition é suportado por uma comunidade de voluntários que doam o seu tempo e conhecimento através da ClearFoundation. Este modelo de suporte pode não ser a melhor solução para o seu negócio ou organização. Por favor, considere todos os fatores (habilidade técnica, número de usuários, a sensibilidade dos dados, recursos etc.) quando considerando a utilização ClearOS Community Edition em uma implantação comercial, profissional, educacional ou em grande escala.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition é ideal para ambientes domésticos onde as pessoas querem mais recursos e serviços em nuvem integrados do que aqueles que normalmente são fornecidos por gateways emitido pelo ISP ou dispositivos de rede home-alvo.';
+$lang['edition_system_registration'] = 'sistema de Registro de';
+$lang['edition_thank_you'] = 'Obrigado por escolher ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade edição';
+$lang['edition_upgrade'] = 'Atualizar';
