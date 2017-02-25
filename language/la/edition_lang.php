f62a1b3448f3d1e2ee3bb90a4d197875ff3aa32a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:20:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Update abort';
+$lang['edition_app_description'] = 'Procurator praebet Editio Vulgata lectio aliqua eligere et mauris in urna.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = 'Visne tu ClearOS ed aliud eligere? Quaeso facito habeas re- praesto subscriptione profiteretur valet ratio. Si incertum, commodo technica sustentatio.';
+$lang['edition_currently_running'] = 'Current version software';
+$lang['edition_downgrade'] = 'downgrade';
+$lang['edition_edition'] = 'edition';
+$lang['edition_exec_summary_business'] = 'Professionally muniret downtime minimize periculum volunt elit diam.';
+$lang['edition_exec_summary_community'] = 'Libera, liberum aeternum. Admins expertus pro Linux aut similia quae ad "tinker".';
+$lang['edition_exec_summary_home'] = 'Auxilium divulgare verbum ClearOS Domus Emendo communicare cum amicis et familia.';
+$lang['edition_no_edition_selected'] = 'Select velis institutionem ed.';
+$lang['edition_no_reset'] = 'Edition reponi non placet liberum contactus quadrigis praesidio tueatur.';
+$lang['edition_not_available_during_testing'] = 'Non credo per alpha / beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'Et nunc iterum Wikipedia adnotatione ad provisionem ratio vestri ratio.';
+$lang['edition_select_edition'] = 'select Edition';
+$lang['edition_summary_business'] = 'Business ClearOS Edition succrevit et est iudicium a ClearCenter Inc. Si vos es vultus pro securitate muniret stabilem gradum, aditum Business editio praeter coetus rebus confirmari et specifica expertus Linux apps admin tincidunt elit et elit.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition voluntariorum communitatem sustinet qui sponte sua et per scientiam ClearFoundation. Hoc exemplum non sit optima solutio pro vestra negotia sustinere et ordo. Vide omnes factores (arte, numerum users, data sensus, vires et cetera) cum pro a usus in commercial ClearOS Community Edition, professionalis, educational vel magna eu elit.';
+$lang['edition_summary_home'] = 'Home Edition ClearOS est specimen pro hominibus domum sphaerae ubi vis unitatem nubes servitia, et magis features, quam ea, quae nulla sunt, si a ISP lata porta vel, iaculis ipsum cogitationes domum.';
+$lang['edition_system_registration'] = 'ratio Registration';
+$lang['edition_thank_you'] = 'Gratias vobis pro Eligentes ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
