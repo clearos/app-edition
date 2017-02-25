25bce6e43c8ec2e15d82889f8322133055f8d5d7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:14:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'kuchotsa Pezani';
+$lang['edition_app_description'] = 'The Manager Edition amapereka ndondomeko kusankha kusankha Baibulo yoyenera chilengedwe ndi bajeti.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = 'Kodi inu mukutsimikiza kuti musankhe ClearOS kope? Chonde onetsetsani kuti ndi muzimvetsera chomveka zilipo kuti adzathe kulembetsa dongosolo. Ngati muli chochita, lemberani luso thandizo.';
+$lang['edition_currently_running'] = 'Current mapulogalamu Baibulo';
+$lang['edition_downgrade'] = '-apansi';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Mwaukadaulo amapereka IT nsanja mabizinesi amene akufuna kuchepetsa chiopsezo downtime.';
+$lang['edition_exec_summary_community'] = 'Free tsopano, free kosatha. Anafuna kuti anakumana admins Linux kapena amene mukufuna "tinker".';
+$lang['edition_exec_summary_home'] = 'Kuthandiza kufalitsa n&#39;kugawana ClearOS Home Edition ndi anzanu kapena achibale.';
+$lang['edition_no_edition_selected'] = 'Chonde sankhani zimene kope mukufuna kukhazikitsa.';
+$lang['edition_no_reset'] = 'kope ili singakhale Yambitsaninso, funsani gulu thandizo thandizo.';
+$lang['edition_not_available_during_testing'] = 'Palibe pa alpha / beta.';
+$lang['edition_not_available'] = 'Sakupezeka.';
+$lang['edition_proceed_to_registration'] = 'Inu tsopano kunam&#39;sintha dongosolo kulembetsa kuti adzathe makonzedwe dongosolo wanu.';
+$lang['edition_select_edition'] = 'Sankhani Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition chimakula ndipo anakhalabe ndi ClearCenter Inc. Ngati mukufuna malo ndizicheza ndi anthu amapereka IT nsanja, ndi Edition Business angapeze mapulogalamu yekha bizinesi yeniyeni ndi imayendetsedwa ndi gulu la anakumana Linux boma, kutukula ndi akatswiri analamula.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition imayendetsedwa ndi gulu la anthu odzipereka kupereka nthawi yawo ndi nzeru kudzera ClearFoundation lapansi. Ichi chitsanzo thandizo si njira yabwino malonda kapena gulu. Taonani zinthu zonse (luso luso, chiwerengero cha Intaneti, deta tilinazo, zinthu etc.) tikaonetsetsa ntchito ClearOS Community Edition mu malonda, ntchito yophunzitsa kapena zikuluzikulu analamula.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition ndi abwino mapangidwe kunyumba kumene anthu amafuna zinthu zambiri ndi ntchito Integrated mtambo kuposa anthu amene amagwiritsidwa operekedwa ndi zipata ISP-chikalata kapena zipangizo kunyumba akulimbana Intaneti.';
+$lang['edition_system_registration'] = 'System Kulembetsa';
+$lang['edition_thank_you'] = 'Zikomo chifukwa Kusankha ClearOS';
+$lang['edition_updating'] = 'Mapulogalamu Pezani mu Patsogolo';
+$lang['edition_upgrade_edition'] = 'Sinthani Edition';
+$lang['edition_upgrade'] = 'Mokweza';
