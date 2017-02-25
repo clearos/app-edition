68dad817e4f202d0e71a2c3d1b2bf556e87bf9ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:26:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = ' Ka kōpo etsa bonnete ba hore o na le phano ya tshebeletso le utloahalang la fumanehang ho botjha ngodisa tsamaiso. Haeba u sa tiisehe, ka kōpo ikopanye tshehetso botekgeniki.';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'khatiso';
+$lang['edition_exec_summary_business'] = 'Professionally supported IT platform for businesses who want to minimize the risk of downtime.';
+$lang['edition_exec_summary_community'] = 'Free hona joale, free ho sa feleng. ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'Ha e fumanehe ka nako ya boing / tsa beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = ' ';
+$lang['edition_summary_community'] = 'ClearOS Community Edition o tšehetsoa ke mokhatlo oa baithaopi ba phallela ka nako ea bona le tsebo dessyecik ClearFoundation eo.  ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition e loketseng bakeng sa ho dibaka lapeng moo batho ka bomong batla ditshebetso tse eketsehileng le ditshebeletso le nyalano hape leru ho feta ba tse tloaelo fanoeng ke gateways ISP-ntša kapa disebediswa lapeng-targeted netweke.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Software Update a Hatele Pele';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
