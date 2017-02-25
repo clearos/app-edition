4c985287e3ce256a68923dac8036aecb76d98b64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:27:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Itigil update';
+$lang['edition_app_description'] = 'Ang Edition Manager ay nagbibigay ng isang seleksyon proseso upang piliin ang tamang bersyon para sa iyong kapaligiran at badyet.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = 'Sigurado ka bang gusto mong pumili ng ibang ClearOS edition? Mangyaring siguraduhin na mayroon kang isang wastong subscription magagamit upang muling magparehistro ang sistema. Kung hindi ka sigurado, mangyaring makipag-ugnay sa teknikal na suporta.';
+$lang['edition_currently_running'] = 'Kasalukuyang bersyon ng software';
+$lang['edition_downgrade'] = 'pagbaba';
+$lang['edition_edition'] = 'edisyon';
+$lang['edition_exec_summary_business'] = 'Propesyonal suportado IT platform para sa mga negosyo na nais na mabawasan ang panganib ng downtime.';
+$lang['edition_exec_summary_community'] = 'Free ngayon, libreng magpakailanman. Inilaan para sa nakaranas admin Linux o mga taong nais na "tinker".';
+$lang['edition_exec_summary_home'] = 'Tumulong ipagkalat ang salita at ibahagi ang ClearOS Home Edition sa iyong mga kaibigan at pamilya.';
+$lang['edition_no_edition_selected'] = 'Mangyaring piliin kung aling mga edisyon na nais mong i-install.';
+$lang['edition_no_reset'] = 'Edisyon na ito ay hindi maaaring maging reset, mangyaring makipag-ugnay sa koponan ng suporta para sa tulong.';
+$lang['edition_not_available_during_testing'] = 'Hindi magagamit sa panahon ng alpha / beta.';
+$lang['edition_not_available'] = 'Hindi magagamit.';
+$lang['edition_proceed_to_registration'] = 'Ikaw ngayon ay nai-redirect sa sistema ng pagpaparehistro upang muling pagkakaloob iyong system.';
+$lang['edition_select_edition'] = 'Piliin Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition ay binuo at pinananatili sa pamamagitan ng ClearCenter Inc. Kung ikaw ay naghahanap para sa isang ligtas, matatag at suportado IT platform, ang Business Edition ay may access sa eksklusibong negosyo-tiyak na apps at ito ay suportado sa pamamagitan ng isang grupo ng mga nakaranas Linux admin, mga developer at paglawak mga espesyalista.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition ay suportado ng isang komunidad ng mga boluntaryo ang pagbibigay ng donasyon ng kanilang oras at kaalaman sa pamamagitan ng ClearFoundation. Ito support modelo ay maaaring hindi ang pinakamahusay na solusyon para sa iyong negosyo o organisasyon. Mangyaring isaalang-alang ang lahat ng mga kadahilanan (teknikal na kasanayan, bilang ng mga gumagamit, ang data sensitivity, resources at iba pa) kung isasaalang-alang ang paggamit ClearOS Community Edition sa isang komersyal na, propesyonal, pang-edukasyon o malakihang deployment.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition ay mainam para sa mga kapaligiran sa bahay kung saan mga indibidwal na nais mas maraming mga tampok at integrated serbisyo ng ulap kaysa sa mga na ay karaniwang ibinibigay ng ISP na ibinigay ng gateways o sa bahay na naka-target network device.';
+$lang['edition_system_registration'] = 'Registration System';
+$lang['edition_thank_you'] = 'Thank You para sa Pagpili ClearOS';
+$lang['edition_updating'] = 'Software Update sa Progress';
+$lang['edition_upgrade_edition'] = 'Mag-upgrade Edition';
+$lang['edition_upgrade'] = 'I-upgrade';
