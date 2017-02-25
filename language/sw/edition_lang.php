a423667689f1e9192e12de1422cbdd6adf7bfd61
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:26:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'mimba Mwisho';
+$lang['edition_app_description'] = 'Meneja Edition hutoa mchakato wa uteuzi kwa kuchagua toleo sahihi kwa mazingira yako na bajeti.';
+$lang['edition_app_name'] = 'Edition Meneja';
+$lang['edition_confirm_edition_change'] = 'Je, una uhakika unataka kuchagua tofauti ClearOS toleo? Tafadhali kuhakikisha una michango halali inapatikana kujisajili upya mfumo. Kama huna uhakika, tafadhali wasiliana na msaada wa kiufundi.';
+$lang['edition_currently_running'] = 'Sasa toleo la programu';
+$lang['edition_downgrade'] = 'downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Kitaaluma mkono IT jukwaa kwa ajili ya biashara ambao wanataka kupunguza hatari ya downtime.';
+$lang['edition_exec_summary_community'] = 'Bure sasa, huru milele. Lengo kwa ajili ya uzoefu admins Linux au wale ambao kama "tinker".';
+$lang['edition_exec_summary_home'] = 'Kusaidia kueneza neno na kushiriki ClearOS Home Edition kwa rafiki yako na familia.';
+$lang['edition_no_edition_selected'] = 'Tafadhali kuchagua ambayo toleo ungependa kufunga.';
+$lang['edition_no_reset'] = 'toleo hili hawezi kuwa upya, tafadhali wasiliana na timu ya msaada kwa ajili ya msaada.';
+$lang['edition_not_available_during_testing'] = 'Si inapatikana wakati alpha / beta.';
+$lang['edition_not_available'] = 'Haipatikani.';
+$lang['edition_proceed_to_registration'] = 'Wewe sasa kuwa itaelekezwa mfumo wa usajili wa re-utoaji mfumo wako.';
+$lang['edition_select_edition'] = 'Kuchagua Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition ni maendeleo na kudumishwa na ClearCenter Inc Kama wewe ni kuangalia kwa salama, imara na mkono IT jukwaa, toleo Business ana upatikanaji wa programu ya kipekee ya biashara maalum na ni mkono na kundi la uzoefu Linux admin, watengenezaji na wataalamu kupelekwa.';
+$lang['edition_summary_community'] = 'ClearOS Jumuiya ya Edition ni mkono na jumuiya ya kujitolea kuchangia muda wao na maarifa kupitia ClearFoundation. Hii mtindo msaada inaweza kuwa suluhisho bora kwa ajili ya biashara yako au shirika. Tafadhali fikiria mambo yote (ujuzi wa kiufundi, idadi ya watumiaji, data unyeti, rasilimali nk) wakati kuzingatia kutumia ClearOS Jumuiya ya Edition katika kibiashara, kitaaluma, elimu au kwa kiasi kikubwa kupelekwa.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition ni bora kwa mazingira ya nyumbani ambapo watu wanataka makala zaidi na huduma za pamoja wingu kuliko wale ambao ni kawaida zinazotolewa na gateways ISP-iliyotolewa au vifaa nyumbani-walengwa mtandao.';
+$lang['edition_system_registration'] = 'System Usajili';
+$lang['edition_thank_you'] = 'Asante kwa Uchaguzi ClearOS';
+$lang['edition_updating'] = 'Software Mwisho katika Maendeleo';
+$lang['edition_upgrade_edition'] = 'kuboresha Edition';
+$lang['edition_upgrade'] = 'Upgrade';
