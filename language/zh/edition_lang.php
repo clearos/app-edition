f7634bc0ac98ec6f2ac6dbfc67c020ea6223e200
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:14:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = '中止更新';
+$lang['edition_app_description'] = '该版Manager提供了一个选择的过程，选择适合您的环境和预算的正确的版本。';
+$lang['edition_app_name'] = '版管理器';
+$lang['edition_confirm_edition_change'] = '您确定要选择不同的ClearOS版？ 请确保你有一个有效的订阅重新注册系统。 如果你不确定，请联系技术支持。';
+$lang['edition_currently_running'] = '当前软件版本';
+$lang['edition_downgrade'] = '下坡';
+$lang['edition_edition'] = '版';
+$lang['edition_exec_summary_business'] = '专业IT支持平台，谁希望尽量减少停机风险的企业。';
+$lang['edition_exec_summary_community'] = '现在免费，永远免费。 适用于经验丰富的Linux管理员或谁喜欢“补锅匠”。';
+$lang['edition_exec_summary_home'] = '帮助流传着一句话，并与您的朋友和家人分享ClearOS家庭版。';
+$lang['edition_no_edition_selected'] = '请选择您要安装哪个版本。';
+$lang['edition_no_reset'] = '此版本不能复位，请与支持团队寻求帮助。';
+$lang['edition_not_available_during_testing'] = 'α/β期间不可用。';
+$lang['edition_not_available'] = '不可用。';
+$lang['edition_proceed_to_registration'] = '现在，您将被重定向到系统注册，重新提供您的系统。';
+$lang['edition_select_edition'] = '选择版';
+$lang['edition_summary_business'] = 'ClearOS商业版是开发和ClearCenter公司维护 如果你正在寻找一个安全，稳定和支持的IT平台，企业版访问专属特定业务应用，并是由一群经验丰富的Linux管理员，开发人员和部署专家的支持。';
+$lang['edition_summary_community'] = 'ClearOS社区版是由志愿者捐赠他们的时间和知识，通过ClearFoundation的社区支持。 这种支持模式可能不适合您的企业或组织的最佳解决方案。 在商业，职业，教育或大规模的部署中使用ClearOS Community Edition的考虑时，请考虑所有因素（技术能力，用户数，数据的敏感性，资源等）。';
+$lang['edition_summary_home'] = 'ClearOS家庭版非常适合于个人希望更多的功能和比那些通常由ISP发行的网关或家庭有针对性的网络设备提供集成的云服务的家庭环境。';
+$lang['edition_system_registration'] = '系统注册';
+$lang['edition_thank_you'] = '感谢您选择ClearOS';
+$lang['edition_updating'] = '在Progress软件更新';
+$lang['edition_upgrade_edition'] = '升级版';
+$lang['edition_upgrade'] = '升级';
