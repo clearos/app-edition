31a12dd4b9d2da82f9e819973c3044533acab7fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:27:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = ' దయచేసి మీరు సిస్టమ్ తిరిగి నమోదు అందుబాటులో సరైన సభ్యత్వ కలిగి నిర్ధారించుకోండి. మీకు తెలియకుంటే, దయచేసి సాంకేతిక మద్దతును సంప్రదించండి.';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'డౌన్గ్రేడ్';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professionally supported IT platform for businesses who want to minimize the risk of downtime.';
+$lang['edition_exec_summary_community'] = 'ఇప్పుడు ఉచిత, ఉచిత ఎప్పటికీ. ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'ఆల్ఫా / బీటా సమయంలో అందుబాటులో లేదు.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = 'ClearOS వ్యాపారం ఎడిషన్ అభివృద్ధి మరియు ClearCenter ఇంక్ ద్వారా నిర్వహించబడుతుంది ';
+$lang['edition_summary_community'] = '  ఒక వాణిజ్య ప్రొఫెషనల్ కోసం, విద్యా లేదా పెద్ద ఎత్తున విస్తరణ లో ClearOS కమ్యూనిటీ ఎడిషన్ ఉపయోగించి పరిగణలోకి దయచేసి అన్ని కారకాలు (సాంకేతిక నైపుణ్యం, వినియోగదారులు సంఖ్య, డేటా సున్నితత్వం, వనరులు మొదలైనవి) భావిస్తారు.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
