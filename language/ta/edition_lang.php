16d7fabe1840e39e85adf04b4b92629e4dec1d03
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:27:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'பதிப்பு மேலாளர்';
+$lang['edition_confirm_edition_change'] = ' நீங்கள் கிடைக்க சரியான சந்தா மீண்டும் பதிவு அமைப்பு உள்ளது என்பதை உறுதி செய்து. ';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professionally supported IT platform for businesses who want to minimize the risk of downtime.';
+$lang['edition_exec_summary_community'] = 'இப்போது இலவச, என்றென்றும் இலவச. ';
+$lang['edition_exec_summary_home'] = 'வார்த்தை பரவியது மற்றும் உதவும் உங்கள் நண்பர்கள் மற்றும் குடும்பத்துடன் ClearOS முகப்பு பதிப்பு பகிர்ந்து.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'கிடைக்கவில்லை.';
+$lang['edition_proceed_to_registration'] = 'நீங்கள் இப்போது ஒதுக்கீடு மீண்டும் உங்கள் கணினியில் கணினி பதிவு திருப்பிவிடப்பட்டது.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = 'ClearOS வணிக பதிப்பு வளர்ந்த மற்றும் ClearCenter இன்க் மூலம் பராமரிக்கப்படுகிறது ';
+$lang['edition_summary_community'] = ' இந்த ஆதரவு மாதிரி உங்கள் வணிக அல்லது அமைப்பு சிறந்த தீர்வு இருக்கலாம். ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'கணினி பதிவு';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'பதிப்பு மேம்படுத்து';
+$lang['edition_upgrade'] = 'மேம்படுத்தல்';
