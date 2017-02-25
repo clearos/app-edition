059fdf198409df3e1d9f09d573ed7cb7a71123a8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:14:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Update ကို abort';
+$lang['edition_app_description'] = 'အဆိုပါ Edition ကို Manager ကိုသင့်ရဲ့ပတ်ဝန်းကျင်နှင့်ဘတ်ဂျက်အဘို့အညာဘက်ကိုဗားရှင်းရှေးခယျြဖို့တစ်ရွေးချယ်ရေးလုပ်ငန်းစဉ်တွေပေးစွမ်းသည်။';
+$lang['edition_app_name'] = 'Edition ကို Manager ကို';
+$lang['edition_confirm_edition_change'] = 'သင်တစ်ဦးကွဲပြားခြားနားသော ClearOS ထုတ်ဝေရွေးဖို့လိုခငျြသေချာလား? သငျသညျ re-register စနစ်မှရရှိနိုင်မယ့်တရားဝင်စာရင်းပေးသွင်းခြင်းရှိသေချာအောင်ပေးပါ။ သငျသညျမသေချာသေးလျှင်, နည်းပညာပံ့ပိုးမှုဆက်သွယ်နိုင်ပါသည်။';
+$lang['edition_currently_running'] = 'လက်ရှိ software ကိုဗားရှင်း';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition ကို';
+$lang['edition_exec_summary_business'] = 'ပညာရှင်ပီသစွာကျချိန်၏အန္တရာယ်ကို minimize ချင်သူအစီးပွားရေးလုပ်ငန်းတွေအတွက်အိုင်တီပလက်ဖောင်းထောက်ခံခဲ့ကြသည်။';
+$lang['edition_exec_summary_community'] = 'အစဉ်အမြဲအခမဲ့, ယခုအခမဲ့ဖြစ်သည်။ အတွေ့အကြုံရှိတဲ့ Linux ကိုစီမံခနျ့ခှဲသို့မဟုတ် "Tinker" လိုသူများအတွက်ရည်ရွယ်ပါသည်။';
+$lang['edition_exec_summary_home'] = 'နှုတ်ကပတ်တော်ကိုဖြန့်နှင့်သင့်မိတ်ဆွေများနှင့်မိသားစုနှင့်အတူ ClearOS Home Edition ကိုမျှဝေကိုကူညီပါ။';
+$lang['edition_no_edition_selected'] = 'သငျသညျကို install လုပ်ဖို့လိုသည့် edition ရွေးချယ်ပါ။';
+$lang['edition_no_reset'] = 'ဒီစာစောင်မှပြန်လည်စတင်မဖွစျနိုငျ, အကူအညီများအတွက်အထောက်အပံ့အဖွဲ့ကိုဆက်သွယ်ပါ။';
+$lang['edition_not_available_during_testing'] = 'alpha / beta ကိုကာလအတွင်းမရရှိနိုင်ပါ။';
+$lang['edition_not_available'] = 'မရရှိနိုင်ပါ။';
+$lang['edition_proceed_to_registration'] = 'သငျသညျယခုသင့် system-ပြဋ္ဌာန်းချက်ပြန်လည်ဖို့ system ကိုမှတ်ပုံတင်ဖို့ redirected လိမ့်မည်။';
+$lang['edition_select_edition'] = 'Edition ကို Select လုပ်ပါ';
+$lang['edition_summary_business'] = 'ClearOS စီးပွားရေး Edition ကို ClearCenter Inc ကိုအားဖြင့်ဖွံ့ဖြိုးပြီးနှင့်ထိန်းသိမ်းထားသည် သင်တစ်ဦး, လုံခြုံတည်ငြိမ်ရေးနှင့်အိုင်တီပလက်ဖောင်းထောက်ခံဘို့ရှာကြသည်ဆိုပါက, စီးပွားရေး Edition ကိုသီးသန့်စီးပွားရေးလုပ်ငန်း-တိကျတဲ့ apps များမှဝင်ရောက်ခွင့်ရှိပြီးအတွေ့အကြုံ Linux ကို admin ရဲ့, developer များနှင့်ဖြန့်ကျက်အထူးကုဆရာဝန်ကြီးများ၏အုပ်စုတစုကထောက်ခံသည်။';
+$lang['edition_summary_community'] = 'ClearOS အဝန်း Edition ကိုအ ClearFoundation မှတဆင့်၎င်းတို့၏အချိန်နှင့်အသိပညာလှူဒါန်းခဲ့စေတနာ့ဝန်ထမ်းတစ်ဦးအသိုင်းအဝိုင်းကထောက်ခံသည်။ ဒီထောက်ခံမှုမော်ဒယ်သင့်လုပ်ငန်းသို့မဟုတ်အဖွဲ့အစည်းများအတွက်အကောင်းဆုံးဖြေရှင်းနည်းဖြစ်မည်မဟုတ်ပါ။ တစ်ဦး, စီးပွားဖြစ်ပရော်ဖက်ရှင်နယ်, ပညာရေးသို့မဟုတ်အကြီးစားဖြန့်ကျက်အတွက် ClearOS အဝန်း Edition ကိုသုံးပြီးစဉ်းစားရသောအခါအားလုံးအချက်များ (နည်းပညာဆိုင်ရာကျွမ်းကျင်မှုအသုံးပြုသူများ, ဒေတာ sensitivity ကို, အရင်းအမြစ်များစသည်တို့ကို၏နံပါတ်) ကိုစဉ်းစားပေးပါ။';
+$lang['edition_summary_home'] = 'ClearOS Home Edition ကိုတဦးချင်းစီပုံမှန်အားဖြင့် ISP က-ထုတ်ပေး gateway များသည်သို့မဟုတ် home-ပစ်မှတ်ထားကွန်ယက်ကိုထုတ်ကုန်ပေးလျက်ရှိသောသူတို့ကိုထက်ပို features နဲ့ဘက်ပေါင်းစုံသည်မိုဃ်းတိမ်န်ဆောင်မှုချင်ဘယ်မှာအိမ်မှာပတ်ဝန်းကျင်မှာများအတွက်စံပြဖြစ်ပါတယ်။';
+$lang['edition_system_registration'] = 'system ကိုမှတ်ပုံတင်ခြင်း';
+$lang['edition_thank_you'] = 'ClearOS ရွေးချယ်ခြင်းသည်အသင့်အားကျေးဇူးတင်ပါသည်';
+$lang['edition_updating'] = 'တိုးတက်မှုအတွက် Software ကို Update ကို';
+$lang['edition_upgrade_edition'] = 'Edition ကိုအဆင့်မြှင့်တင်ရန်';
+$lang['edition_upgrade'] = 'upgrade';
