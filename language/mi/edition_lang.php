055b31868e45d39f00be546157f53d6ec44343dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:21:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'haukoti Whakahōu';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Kaiwhakahaere Edition';
+$lang['edition_confirm_edition_change'] = '  Ki te kore koe e mōhio, tēnā whakapā tautoko hangarau.';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Ngaio tautoko tüäpapa IT mō pakihi e hiahia ana ki te whakaiti i te tūponotanga o te downtime.';
+$lang['edition_exec_summary_community'] = ' ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'Ka inaianei koe kia tukuna ki te rēhitatanga pūnaha ki te anō-kai koutou pūnaha.';
+$lang['edition_select_edition'] = 'Tīpakohia Edition';
+$lang['edition_summary_business'] = ' ';
+$lang['edition_summary_community'] = 'tautoko ClearOS Community Edition e te hapori o te tūao foaki ratou wa, me te matauranga mā te ClearFoundation.  Koa whakaaro āhuatanga katoa (pūkenga hangarau, maha o ngā kaiwhakamahi, tūtohutanga raraunga, rauemi etc) ka whakaaro te whakamahi i ClearOS Community Edition i roto i te whakamahinga arumoni, ngaio, mātauranga ranei nui-tauine.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Whakahōu Pūmanawa i roto i te Kaunuku';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
