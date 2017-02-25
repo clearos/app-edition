22c8d91d73e134d4ff36048544e147efcdc39bf7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:29:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = '  ';
+$lang['edition_currently_running'] = 'Lọwọlọwọ software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Agbejoro ni atilẹyin IT Syeed fun owo ti o fẹ lati gbe awọn ewu ti downtime.';
+$lang['edition_exec_summary_community'] = 'Free bayi, free lailai. ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'Yi àtúnse ko le jẹ ipilẹ, jọwọ kan si awọn support egbe fun iranlowo.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = ' ';
+$lang['edition_summary_community'] = ' Yi support model ko le wa ni ti o dara ju ojutu fun owo rẹ tabi agbari. ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'O ṣeun fun yiyan ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
