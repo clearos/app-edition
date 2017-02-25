832e2ed6fb31e359006e2e1a93d9da595f9fe576
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:19:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Жаңарту доғару';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition менеджері';
+$lang['edition_confirm_edition_change'] = ' Сіз қайта тіркеуден жүйесі үшін қол жетімді жарамды жазылым бар екенін тексеріңіз. ';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Кәсіби тұрып қаупін азайту үшін келеді бизнес үшін АТ платформасын қолдау.';
+$lang['edition_exec_summary_community'] = ' ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'Бұл басылым қалпына келтіру мүмкін емес, көмек алу үшін қолдау көрсету тобына хабарласыңыз.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition ClearCenter Inc. әзірлеген және сақталады ';
+$lang['edition_summary_community'] = 'ClearOS Community Edition ClearFoundation арқылы өз уақыты мен білімін донорлық еріктілер қоғамдастық қолдады.  а, коммерциялық кәсіптік, бiлiм беру немесе кең ауқымды орналастыруында ClearOS Қауымдастық шығарылымын пайдаланып қараған кезде барлық факторлар (техникалық шеберлігі, пайдаланушылар, деректер сезімталдығы, ресурстарды және т.б. саны) ескеріңіз.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Орындалып жатқан Software Update';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Жаңалау';
