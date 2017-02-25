6433572aee7fd513c8e2b2a1fba3560469b24b7d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:15:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Afbryd opdater';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = '  Hvis du er usikker, skal du kontakte teknisk support.';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professionally supported IT platform for businesses who want to minimize the risk of downtime.';
+$lang['edition_exec_summary_community'] = ' Beregnet til erfarne Linux admins eller dem, der kan lide at "pille".';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'Denne udgave kan ikke nulstilles, skal du kontakte supportteamet for assistance.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = ' Hvis du er på udkig efter et sikkert, stabilt og støttet IT-platform, Business Edition har adgang til eksklusive business-specifikke apps og er støttet af en gruppe af erfarne Linux admin, udviklere og implementering specialister.';
+$lang['edition_summary_community'] = '  ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registrering';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
