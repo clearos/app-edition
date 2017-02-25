83635eb671a9f54a0dcacfa8b2a9a8ac96099fb7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:13:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abortatu Update';
+$lang['edition_app_description'] = 'Edizioa kudeatzaileak hautaketa prozesu bat eskuineko bertsioa zure ingurumena eta aurrekontu aukeratu ditu.';
+$lang['edition_app_name'] = 'edizioa Manager';
+$lang['edition_confirm_edition_change'] = 'Ziur ezberdinak ClearOS edizio bat hautatu nahi duzula? Ziurtatu baliozko harpidetza bat erabilgarri berriro erregistratu sistema behar duzu. Ziur ez bazaude, mesedez jarri harremanetan laguntza teknikoa.';
+$lang['edition_currently_running'] = 'Oraingo software bertsioa';
+$lang['edition_downgrade'] = 'Bertsio beheratu';
+$lang['edition_edition'] = 'edizioa';
+$lang['edition_exec_summary_business'] = 'Profesionalki onartzen IT plataforma downtime arriskua minimizatu nahi duten enpresei.';
+$lang['edition_exec_summary_community'] = 'Free orain, free betirako. bizi Linux kudeatzaileek edo nahi duten "tinker" dutenentzat pentsatua.';
+$lang['edition_exec_summary_home'] = 'Laguntza hitza zabaldu eta partekatzeko ClearOS Home Edition zure lagunekin eta familiarekin.';
+$lang['edition_no_edition_selected'] = 'Mesedez aukeratu edizioan instalatu nahi duzun.';
+$lang['edition_no_reset'] = 'edizio hau ezin da berrezarri, mesedez, jarri harremanetan laguntza taldeak eta laguntza eskatzea.';
+$lang['edition_not_available_during_testing'] = 'Ez alpha / beta zehar eskuragarri.';
+$lang['edition_not_available'] = 'Ez dago erabilgarri.';
+$lang['edition_proceed_to_registration'] = 'egingo duzu orain sistema erregistroa birbideratuko-xedapen berriro zure sisteman.';
+$lang['edition_select_edition'] = 'Aukeratu Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edizioa garatzen da eta ClearCenter Inc. mantendutako duzun segurua, egonkorra eta onartzen IT plataforma bat bilatzen baduzu, Business Edizioa esklusiboa enpresa-partida aplikazioetarako sarbidea dauka eta bizi Linux admin, sustatzaile eta hedapena espezialista talde baten babespean.';
+$lang['edition_summary_community'] = 'ClearOS Erkidegoko Edizioa bolondresak beren denbora eta ezagutza emanez ClearFoundation bidez komunitate bat onartzen. laguntza-eredu hori agian ez da zure enpresa edo erakundearen irtenbide onena izan. Mesedez, kontuan faktore guztiak (teknikoa, erabiltzaile kopurua, datuen sentsibilitatea, baliabideak eta abar) denean ClearOS Erkidegoko Edition erabiliz merkataritza, profesionala, hezkuntza edo eskala handiko inplementazio bat kontuan hartuta.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition etxeetarako non gizabanako nahi ezaugarri gehiago eta hori normalean ISP-emana atebide edo home-norakoak sareko gailuak emandako direnak baino hodei integratu zerbitzu ezin hobea da.';
+$lang['edition_system_registration'] = 'Sistema Izen-ematea';
+$lang['edition_thank_you'] = 'Eskerrik asko ClearOS aukeratzea';
+$lang['edition_updating'] = 'Software Update Zinema';
+$lang['edition_upgrade_edition'] = 'Berritu Edition';
+$lang['edition_upgrade'] = 'Upgrade';
