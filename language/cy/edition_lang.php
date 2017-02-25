da65c8ccfe31066163321dfb46ed69ef27710761
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:29:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Rheolwr argraffiad';
+$lang['edition_confirm_edition_change'] = ' Gwnewch yn siŵr fod gennych danysgrifiad dilys sydd ar gael i ail-gofrestru&#39;r system. ';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'argraffiad';
+$lang['edition_exec_summary_business'] = 'Professionally supported IT platform for businesses who want to minimize the risk of downtime.';
+$lang['edition_exec_summary_community'] = 'Am ddim nawr, yn rhad ac am byth. ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Dewiswch pa rhifyn hoffech ei osod.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'Ddim ar gael yn ystod alpha / beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = 'ClearOS Argraffiad Busnes yn cael ei ddatblygu a&#39;i gynnal gan ClearCenter Inc. ';
+$lang['edition_summary_community'] = 'Argraffiad Cymunedol ClearOS cael ei gefnogi gan gymuned o wirfoddolwyr rhoi o&#39;u hamser a&#39;u gwybodaeth drwy gyfrwng y ClearFoundation.  ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Diolch am ddewis ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'uwchraddio';
