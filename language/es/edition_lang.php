471f68d6f7bb719b074aeedac482aae446c655f5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:26:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = '¿Seguro que desea seleccionar una edición diferente ClearOS?  ';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edición';
+$lang['edition_exec_summary_business'] = 'Professionally supported IT platform for businesses who want to minimize the risk of downtime.';
+$lang['edition_exec_summary_community'] = 'Libre ahora, libre para siempre. ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'No disponible.';
+$lang['edition_proceed_to_registration'] = 'Ahora va a ser redirigido a un registro del sistema para volver a su sistema de provisión.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = ' ';
+$lang['edition_summary_community'] = ' Este modelo de apoyo puede no ser la mejor solución para su empresa u organización. ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'edición de la mejora';
+$lang['edition_upgrade'] = 'Upgrade';
