c2b54060455f3d8e44ce48d6a1e85df1794edda8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:29:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = ' Hãy chắc chắn rằng bạn có một thuê bao hợp lệ có sẵn để đăng ký lại hệ thống. ';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Chuyên nghiệp hỗ trợ nền tảng CNTT cho các doanh nghiệp muốn giảm thiểu nguy cơ thời gian chết.';
+$lang['edition_exec_summary_community'] = 'Miễn phí bây giờ, miễn phí mãi mãi. ';
+$lang['edition_exec_summary_home'] = 'Giúp lây lan từ và chia sẻ ClearOS Home Edition với bạn bè và gia đình của bạn.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'This edition cannot be reset, please contact the support team for assistance.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Không có sẵn.';
+$lang['edition_proceed_to_registration'] = 'You will now be redirected to system registration to re-provision your system.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = ' ';
+$lang['edition_summary_community'] = 'ClearOS Community Edition được hỗ trợ bởi một cộng đồng những người tình nguyện hiến thời gian và kiến ​​thức của mình thông qua các ClearFoundation.  ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition is ideal for home environments where individuals want more features and integrated cloud services than those that are typically provided by ISP-issued gateways or home-targeted network devices.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Thank You for Choosing ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'Upgrade';
