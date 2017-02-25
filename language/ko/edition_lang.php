690f2ab11c6510d16714f9ef76fc93261d391cf3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:20:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = '업데이트를 중단';
+$lang['edition_app_description'] = '에디션 관리자는 환경과 예산에 맞는 버전을 선택하는 선택 과정을 제공합니다.';
+$lang['edition_app_name'] = '에디션 관리자';
+$lang['edition_confirm_edition_change'] = '당신은 당신이 다른 ClearOS 버전을 선택 하시겠습니까? 시스템을 재 등록 할 수 유효한 구독이 있는지 확인하십시오. 당신이 확실하지 않은 경우, 기술 지원에 문의하십시오.';
+$lang['edition_currently_running'] = '현재의 소프트웨어 버전';
+$lang['edition_downgrade'] = '내리 받이';
+$lang['edition_edition'] = '판';
+$lang['edition_exec_summary_business'] = '전문적으로 다운 타임의 위험을 최소화하려는 기업을위한 IT 플랫폼을 지원했다.';
+$lang['edition_exec_summary_community'] = '지금 무료, 영원히 무료로 제공됩니다. 경험 리눅스 관리자 또는 "어설프게"에 좋아하는 사람들을위한 것.';
+$lang['edition_exec_summary_home'] = '단어를 확산 도움말 및 당신의 친구 및 가족과 ClearOS Home Edition을 공유 할 수 있습니다.';
+$lang['edition_no_edition_selected'] = '설치하고자하는 버전을 선택하십시오.';
+$lang['edition_no_reset'] = '이 개정판은 재설정 할 수 없습니다, 지원에 대한 지원 팀에 문의하시기 바랍니다.';
+$lang['edition_not_available_during_testing'] = '알파 / 베타 기간 동안 사용할 수 없습니다.';
+$lang['edition_not_available'] = '사용 불가.';
+$lang['edition_proceed_to_registration'] = '이제 시스템을 프로비저닝을 다시 시스템 등록에 리디렉션됩니다.';
+$lang['edition_select_edition'] = 'Edition을 선택';
+$lang['edition_summary_business'] = 'ClearOS 비즈니스 버전을 개발하고 ClearCenter 주식 회사에 의해 유지된다 당신이 안전하고 안정적으로 지원하는 IT 플랫폼을 찾고 있다면, 비즈니스 에디션은 독점적 인 사업 특정 응용 프로그램에 액세스 할 수있는 지식과 경험 리눅스 관리자, 개발자 및 배포 전문가 그룹에 의해 지원됩니다.';
+$lang['edition_summary_community'] = 'ClearOS 커뮤니티 에디션은 ClearFoundation를 통해 자신의 시간과 지식을 기부 자원 봉사자 커뮤니티에 의해 지원됩니다. 이러한 지원 모델은 귀하의 비즈니스 또는 조직에 가장 적합한 솔루션이 될 수 없습니다. A, 상업 전문 교육 또는 대규모 배포 ClearOS 커뮤니티 에디션을 사용하여 고려할 때 모든 요소 (기술 능력, 사용자 수, 데이터 감도, 자원 등)을 고려하시기 바랍니다.';
+$lang['edition_summary_home'] = 'ClearOS 홈 에디션은 개인이 더 많은 기능과 일반적으로 ISP 발급 게이트웨이 나 집에서 대상 네트워크 장치에서 제공하는 것보다 통합 된 클라우드 서비스를 원하는 가정 환경에 이상적입니다.';
+$lang['edition_system_registration'] = '시스템 등록';
+$lang['edition_thank_you'] = 'ClearOS를 선택해 주셔서 감사합니다';
+$lang['edition_updating'] = '진행 중 소프트웨어 업데이트';
+$lang['edition_upgrade_edition'] = '버전 업그레이드';
+$lang['edition_upgrade'] = '업그레이드';
