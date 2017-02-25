790067fb8e424e5587711c1578cc3dea320531ba
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:18:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = ' Si prega di assicurarsi di avere un abbonamento valido a disposizione per registrare nuovamente il sistema. ';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'degradare';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professionally supported IT platform for businesses who want to minimize the risk of downtime.';
+$lang['edition_exec_summary_community'] = ' ';
+$lang['edition_exec_summary_home'] = 'Help spread the word and share ClearOS Home Edition with your friends and family.';
+$lang['edition_no_edition_selected'] = 'Please select which edition you would like to install.';
+$lang['edition_no_reset'] = 'Questa edizione non può essere resettato, si prega di contattare il team di supporto per l&#39;assistenza.';
+$lang['edition_not_available_during_testing'] = 'Not available during alpha/beta.';
+$lang['edition_not_available'] = 'Not available.';
+$lang['edition_proceed_to_registration'] = 'Ora verrà reindirizzato alla registrazione del sistema di ri-disposizione il sistema.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition è sviluppato e mantenuto da ClearCenter Inc. ';
+$lang['edition_summary_community'] = 'ClearOS Community Edition è supportato da una comunità di volontari che donano il loro tempo e la conoscenza attraverso il ClearFoundation.  ';
+$lang['edition_summary_home'] = 'ClearOS Home Edition è ideale per ambienti domestici in cui gli individui vogliono più funzioni e servizi cloud integrati rispetto a quelli che sono in genere forniti da gateway ISP-rilasciato o dispositivi di rete in casa mirato.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Grazie per la scelta ClearOS';
+$lang['edition_updating'] = 'Software Update in Progress';
+$lang['edition_upgrade_edition'] = 'Upgrade Edition';
+$lang['edition_upgrade'] = 'aggiornamento';
