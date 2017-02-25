38cac8610805db1055174b9e052c90b6e08e30c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:17:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'QDialogButtonBox Update';
+$lang['edition_app_description'] = 'The Edition Manager na samar da wani tsari selection zabi da hakkin version for your yanayi da kuma kasafin kudin.';
+$lang['edition_app_name'] = 'edition Manager';
+$lang['edition_confirm_edition_change'] = 'Ka tabbata kana so ka zaži wani daban-daban ClearOS edition? Don Allah ka tabbata kana da wata m biyan kuɗi samuwa ga sake rajistar cikin tsarin. Idan kun kasance m, tuntuɓi goyon bayan sana&#39;a.';
+$lang['edition_currently_running'] = 'Current software version';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'edition';
+$lang['edition_exec_summary_business'] = 'Fasaha da goyan IT dandamali ga harkokin kasuwanci da suke so su rage girman hadarin downtime.';
+$lang['edition_exec_summary_community'] = 'Free yanzu, free har abada. Akayi nufi don dandana Linux admins kõ kuwa waɗanda suke so su "tinker".';
+$lang['edition_exec_summary_home'] = 'Taimake yada kalmar da kuma raba ClearOS Home Edition tare da abokai da iyali.';
+$lang['edition_no_edition_selected'] = 'Don Allah zaži wanda edition ka so a shigar.';
+$lang['edition_no_reset'] = 'Wannan edition ba zai iya zama sake saiti, ka tuntuɓi da goyon bayan tawagar taimako.';
+$lang['edition_not_available_during_testing'] = 'Ba samuwa a lokacin alpha / beta.';
+$lang['edition_not_available'] = 'Babu.';
+$lang['edition_proceed_to_registration'] = 'Za ka yanzu a miƙa ka zuwa tsarin rajista sake arziki tsarin.';
+$lang['edition_select_edition'] = 'Select Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition ne ci gaba da kiyaye da ClearCenter Inc. Idan kana neman wani amintacce, barga, kuma Ya taimake IT dandamali, Business Edition yana damar yin amfani da m business-takamaiman apps da aka goyan bayan wani rukuni na dandana Linux admin, developers da tura kwararru.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition ne goyan bayan da wata al&#39;umma daga masu sa kai taimakawa da lokaci da ilmi via da ClearFoundation. Wannan goyon bayan model bazai mafi kyau bayani for your kasuwanci ko kungiyar. Don Allah duba duk abubuwan (fasaha fasaha, yawan masu amfani, data ji na ƙwarai, albarkatun da dai sauransu) a lokacin da la&#39;akari da amfani da ClearOS Community Edition a kasuwanci, sana&#39;a, ilimi, ko manyan sikelin-girke.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition ne manufa domin home muhallin inda mutane ke so more fasali da kuma Hadakar girgije ayyuka fiye da wadanda aka yawanci bayar da ISP-bayar mashigar ko gida-niyya cibiyar na&#39;urorin.';
+$lang['edition_system_registration'] = 'System Registration';
+$lang['edition_thank_you'] = 'Na gode maka don Zabar ClearOS';
+$lang['edition_updating'] = 'Software Update a Ci gaban';
+$lang['edition_upgrade_edition'] = 'Inganci Edition';
+$lang['edition_upgrade'] = 'inganci';
