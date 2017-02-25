df799d3763fe1d6e0ef36378f663d44425996ee0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:18:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Tobscoir Nuashonraigh';
+$lang['edition_app_description'] = 'Cuireann an Bainisteoir Edition próiseas roghnóireachta a roghnú an leagan ceart do do thimpeallacht agus buiséid.';
+$lang['edition_app_name'] = 'Bainisteoir Edition';
+$lang['edition_confirm_edition_change'] = 'An bhfuil tú cinnte gur mian leat a roghnú eagrán ClearOS difriúil? Bí cinnte go bhfuil tú síntiús bailí ar fáil athchlárú an gcóras. Mura bhfuil tú cinnte, téigh i dteagmháil le tacaíocht theicniúil.';
+$lang['edition_currently_running'] = 'leagan bogearraí atá ann faoi láthair';
+$lang['edition_downgrade'] = 'íosghrádú';
+$lang['edition_edition'] = 'eagrán';
+$lang['edition_exec_summary_business'] = 'tacaíocht phroifisiúnta cláir TF le haghaidh ghnóthaí ar mian leo a chur ar an mbaol downtime a íoslaghdú.';
+$lang['edition_exec_summary_community'] = 'Saor in aisce anois, saor in aisce go deo. Atá beartaithe le haghaidh admins Linux taithí acu nó iad siúd atá ag iarraidh a "tinker".';
+$lang['edition_exec_summary_home'] = 'Cabhair scaipeadh agus ClearOS Baile Edition roinnt le do chairde agus do mhuintir.';
+$lang['edition_no_edition_selected'] = 'Roghnaigh rud a Eagrán mhaith leat a shuiteáil.';
+$lang['edition_no_reset'] = 'Ní féidir eagrán seo a athshocrú, téigh i dteagmháil leis an bhfoireann tacaíochta do chúnamh.';
+$lang['edition_not_available_during_testing'] = 'Níl sé ar fáil le linn alfa / beta.';
+$lang['edition_not_available'] = 'Níl sé ar fáil.';
+$lang['edition_proceed_to_registration'] = 'beidh tú a atreorú anois le clárú an chórais a ath-sholáthar do chóras.';
+$lang['edition_select_edition'] = 'Roghnaigh Edition';
+$lang['edition_summary_business'] = 'ClearOS Gnó Eagrán á fhorbairt agus á chothabháil ag ClearCenter Inc. Má tá tú ag lorg ar ardán daingean, seasmhach agus tacaíocht TF, tá rochtain ar apps gnó a bhaineann go sonrach eisiach an Edition Gnó agus tacaíonn grúpa de taithí Linux admin, forbróirí agus speisialtóirí imlonnaithe.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition Tacaíonn pobal na oibrithe deonacha bhronnadh gcuid ama agus eolais tríd an ClearFoundation. Ní féidir an múnla tacaíochta a bheith ar an réiteach is fearr do do ghnó nó eagraíocht. Iarrtar ort smaoineamh ar na fachtóirí (scil teicniúil, líon na n-úsáideoirí, íogaireacht sonraí, acmhainní srl) ag baint úsáide as ClearOS Community Edition in imscaradh tráchtála, gairmiúla, oideachais nó ar scála mór nuair a bhreithniú.';
+$lang['edition_summary_home'] = 'Is ClearOS Baile Edition iontach do timpeallachtaí baile nuair is mian le daoine aonair gnéithe níos mó agus seirbhísí scamall comhtháite ná iad siúd atá curtha ar fáil de ghnáth ag gheataí ISP-eisithe nó feistí líonra bhaile-spriocdhírithe.';
+$lang['edition_system_registration'] = 'Clárú córas';
+$lang['edition_thank_you'] = 'Go raibh maith agat as ClearOS Roghnú';
+$lang['edition_updating'] = 'Nuashonrú Bogearraí Lámha';
+$lang['edition_upgrade_edition'] = 'Uasghrádú Edition';
+$lang['edition_upgrade'] = 'uasghrádú';
