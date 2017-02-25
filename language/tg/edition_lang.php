3ceb4e5ec695b8e6e106b6d3cefd76798297324a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:27:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'The Edition Manager provides a selection process to choose the right version for your environment and budget.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = 'Оё мутмаинед, ки мехоҳед интихоб кунед нашри гуногуни ClearOS? Боварӣ ҳосил намоед, ки шумо обуна дуруст дастрас аз нав ба қайд система. Агар шумо намедонед ҳастед, лутфан ба дастгирии техникии.';
+$lang['edition_currently_running'] = 'версияи ҷории нармафзор';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Касбӣ платформаи IT барои бизнес, ки мехоҳанд ба кам кардани хатари бозистии дастгирӣ карда мешавад.';
+$lang['edition_exec_summary_community'] = 'Озод ҳоло, озод абад. Барои admins Linux сар ва ё касоне, ки дӯст ба «tinker».';
+$lang['edition_exec_summary_home'] = 'Кӯмак паҳн кардани калима ва мубодила ClearOS Home Edition бо оила ва рафиқони худ.';
+$lang['edition_no_edition_selected'] = 'Лутфан, интихоб кунед, ки нашри шумо мехоҳам ба насб.';
+$lang['edition_no_reset'] = 'Нашри мумкин нест, аз нав, лутфан барои кўмаки тамос дастаи дастгирии.';
+$lang['edition_not_available_during_testing'] = 'дар давоми алфа / бета дастрас нест.';
+$lang['edition_not_available'] = 'Дастрас нест.';
+$lang['edition_proceed_to_registration'] = 'Шумо ҳоло ба қайд гирифта системаи равона карда шавад, то дубора таъмини системаи шумо.';
+$lang['edition_select_edition'] = '-ро интихоб кунед Edition';
+$lang['edition_summary_business'] = 'ClearOS нашри соҳибкорӣ таҳия ва нигоҳдорӣ аз ҷониби ClearCenter Inc. Агар шумо ҷустуҷӯ барои платформаи эмин, устувор ва дастгирии иттилоотӣ, нашри Business дастрасӣ ба барномаҳои-бизнес мушаххас истисноӣ дорад ва аз ҷониби як гурӯҳи сар администратор Linux, таҳиягарон ва мутахассисони густариши дастгирӣ карда мешавад.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition аз ҷониби ҷомеаи ихтиёриён donating вақт ва дониши худро тавассути ClearFoundation дастгирӣ карда мешавад. Ин модели дастгирии метавонад беҳтарин роҳи ҳалли барои бизнес ва ё созмони шумо нест. Марҳамат карда ҳамаи омилҳо (маҳорат техникӣ, шумораи истифодабарандагони, ҳассосияти маълумот, захираҳои ва ғайра) ҳангоми баррасии истифодаи ClearOS Community Edition дар густариши тиҷоратӣ, касбӣ, таълимӣ ва ё-миқёси калон дида бароем.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition беҳтарин барои муҳити хона, ки шахсони мехоҳанд хусусиятҳои бештар ва хизматрасонии абр маҷмӯӣ аз онон, ки маъмулан аз ҷониби gateways-додашуда ISP ё дастгоҳҳои шабакаи-хона мақсадноки пешкаш кардааст.';
+$lang['edition_system_registration'] = 'Бақайдгирии Системаи';
+$lang['edition_thank_you'] = 'Ташаккур ба шумо барои Интихоби ClearOS';
+$lang['edition_updating'] = 'Навсозии нармафзор дар Пешрафт';
+$lang['edition_upgrade_edition'] = 'навсозӣ Edition';
+$lang['edition_upgrade'] = 'навсозӣ';
