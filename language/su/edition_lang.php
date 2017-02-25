03068b1cc1bb156a5290a67a367d09418387c027
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 02:26:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/edition_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['edition_abort_update'] = 'Abort Update';
+$lang['edition_app_description'] = 'Nu Edition Manager nyadiakeun prosés seléksi milih versi katuhu pikeun lingkungan jeung anggaran.';
+$lang['edition_app_name'] = 'Edition Manager';
+$lang['edition_confirm_edition_change'] = 'Dupi anjeun yakin rek milih édisi ClearOS beda? Mangga pastikeun Anjeun gaduh langganan valid sadia pikeun ulang ngadaptar-sistem. Mun anjeun unsure, mangga kontak pangrojong téhnis.';
+$lang['edition_currently_running'] = 'versi sofwer Wayah';
+$lang['edition_downgrade'] = 'Downgrade';
+$lang['edition_edition'] = 'Edition';
+$lang['edition_exec_summary_business'] = 'Professionally dirojong platform IT keur usaha anu hayang ngaleutikan risiko downtime.';
+$lang['edition_exec_summary_community'] = 'Free ayeuna, bebas salawasna. Dimaksudkeun pikeun admins Linux ngalaman atawa jalma anu resep ka "tinker".';
+$lang['edition_exec_summary_home'] = 'Pitulung sumebar kecap sarta babagi ClearOS Home Edition jeung babaturan jeung kulawarga.';
+$lang['edition_no_edition_selected'] = 'Mangga pilih nu édisi Anjeun hoyong masang.';
+$lang['edition_no_reset'] = 'édisi ieu teu bisa reset, mangga kontak tim pangrojong pikeun bantuan.';
+$lang['edition_not_available_during_testing'] = 'Teu sadia salila alfa / béta.';
+$lang['edition_not_available'] = 'Henteu sayogi.';
+$lang['edition_proceed_to_registration'] = 'Anjeun ayeuna bakal dialihkeun ka ngadaptar sistim ka ulang rezeki sistim Anjeun.';
+$lang['edition_select_edition'] = 'pilih Edition';
+$lang['edition_summary_business'] = 'ClearOS Business Edition téh dimekarkeun jeung dijaga ku ClearCenter Inc. Upami Anjeun keur néangan hiji platform IT aman, stabil sarta dirojong, nu Edition Business ngabogaan aksés ka aktip husus-bisnis ekslusif sarta dirojong ku grup ngalaman admin Linux, pamekar jeung spesialis deployment.';
+$lang['edition_summary_community'] = 'ClearOS Community Edition dirojong ku masarakat sukarelawan nyumbangkeun waktu jeung pangaweruh maranéhanana via ClearFoundation teh. model pangrojong ieu bisa jadi teu leyuran pangalusna pikeun bisnis atawa organisasi. Mangga mertimbangkeun sadaya faktor (skill téhnis, jumlah pamaké, sensitipitas data, sumber jsb) lamun tempo ngagunakeun ClearOS Community Edition dina komérsial, panarjamah, atikan atawa gede skala deployment.';
+$lang['edition_summary_home'] = 'ClearOS Home Edition téh idéal pikeun lingkungan imah mana individu hayang leuwih fitur sarta ladenan awan terpadu ti maranéhanana anu ilaharna disadiakeun ku gateways ISP-dikaluarkeun atawa alat jaringan asal-sasaran.';
+$lang['edition_system_registration'] = 'Pendaftaran System';
+$lang['edition_thank_you'] = 'Hatur Anjeun pikeun Milih ClearOS';
+$lang['edition_updating'] = 'Software Update dina kamajuan';
+$lang['edition_upgrade_edition'] = 'ningkatkeun Edition';
+$lang['edition_upgrade'] = 'pamutahiran';
